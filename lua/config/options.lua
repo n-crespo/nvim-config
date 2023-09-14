21efe1c743c2824f7797cc68215f3a0930526360
nicolas <nicolasecrespo@gmail.com>
2023-09-14 17:27:09 -0400

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,35 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+opt.autowrite = true -- Enable auto writes
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+opt.conceallevel = 3 -- Hide * markup for bold and italics
+opt.cursorline = true
+opt.number = true
+opt.relativenumber = true
+-- opt.winbar = "%=%m %f"
+
+print("hello")
+opt.splitbelow = true
+opt.splitright = true
+opt.swapfile = false
+opt.wrap = false
+opt.incsearch = true
+opt.scrolloff = 8
+opt.relativenumber = true
+opt.numberwidth = 1
+
+-- vim.g.dashboard_custom_header = {
+--   [[                                                                       ]],
+--   [[                                                                     ]],
+--   [[       ████ ██████           █████      ██                     ]],
+--   [[      ███████████             █████                             ]],
+--   [[      █████████ ███████████████████ ███   ███████████   ]],
+--   [[     █████████  ███    █████████████ █████ ██████████████   ]],
+--   [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+--   [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+--   [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+--   [[                                                                       ]],
+-- }
---
+++ lua/plugins/rmd.lua
@@ -0,0 +1,10 @@
+-- add functionality for rmarkdown
+return {
+  {
+    "vim-pandoc/vim-rmarkdown",
+    dependencies = {
+      "vim-pandoc/vim-pandoc",
+      "vim-pandoc/vim-pandoc-syntax",
+    },
+  },
+}
