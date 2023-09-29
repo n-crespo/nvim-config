61055ba8448a554b7f94a0a90a6535d09c13236b
nicolas <nicolasecrespo@gmail.com>
2023-09-29 07:51:44 -0400

    Update Alpha and added plugin README generator

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -18,8 +18,8 @@ return {
       dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
       -- dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
       dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-      dashboard.button("h", "󰗶 " .. " Get Healthy", ":checkhealth <CR>"),
-      dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load()<CR>]]),
+      -- dashboard.button("h", "󰗶 " .. " Get Healthy", ":checkhealth <CR>"),
+      dashboard.button("s", " " .. " Restore Session", ":lua require('persistence').load()<CR>"),
       dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
       dashboard.button("q", " " .. " Quit", ":qa<CR>"),
     }
--- lua/plugins/mdmaker.lua
+++ lua/plugins/mdmaker.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     nvim_dir = "~/.config/nvim/",
     output = "~/.config/nvim/lua/plugins/README.md",
-    enable_url_check = true, -- disable if generating README.md offline or with a bad connection
+    enable_url_check = false, -- disable if generating README.md offline or with a bad connection
     package_maganer = "folke/lazy.nvim",
     -- If you don't want any of the following fields, set them to ""
     title = "Neovim Configuration",
