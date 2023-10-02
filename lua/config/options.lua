768b5bb470a003b47bafb8b24ab02867e43d21a2
nicolas <nicolasecrespo@gmail.com>
2023-10-01 20:56:42 -0400

    added writing plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,8 +6,8 @@ local opt = vim.opt
 opt.autowrite = true -- Enable auto writes
 opt.clipboard = "unnamedplus" -- Sync with system clipboard
 opt.conceallevel = 3 -- Hide * markup for bold and italics
-opt.cursorline = true
 opt.number = true
+opt.cursorline = false
 opt.relativenumber = true
 -- opt.winbar = "%=%m %f"
 
@@ -18,5 +18,5 @@ opt.swapfile = false
 opt.wrap = false
 opt.incsearch = true
 opt.scrolloff = 8
-opt.relativenumber = true
 opt.numberwidth = 1
+opt.relativenumber = true
