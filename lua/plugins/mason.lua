43a7893150428e09d173d6eab16750493ab01687
nicolas <nicolasecrespo@gmail.com>
2023-11-17 00:09:25 -0500

    removed garbage day and plugin README, updated Mason ensure installed list

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,19 +2,28 @@ return {
   "williamboman/mason.nvim",
   opts = {
     ensure_installed = {
+      "arduino-language-server",
       "bash-language-server",
       "black",
+      "cbfmt",
       "clang-format",
       "clangd",
       "flake8",
       "jdtls",
       "json-lsp",
       "lua-language-server",
+      "markdownlint",
+      "marksman",
+      "mdformat",
       "pyright",
+      "r-languageserver"
+      "ruff-lsp",
       "rust-analyzer",
       "shellcheck",
       "shfmt",
       "stylua",
-    },
+   },
   },
 }
+
+
