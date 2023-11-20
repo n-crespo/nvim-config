79232fd863a7a540443b943267d8276219d38f47
Nicolas <nicolasecrespo@gmail.com>
2023-11-20 08:41:29 -0500

    plugin updates and trying to fix markdown preview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,4 +1,12 @@
 return {
   { "ixru/nvim-markdown" },
-  -- markdown preview is loaded through lazy extras
+  -- markdown preview is loaded through lazy extras (but it's having problems...)
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  --   ft = { "markdown" },
+  --   build = function()
+  --     vim.fn["mkdp#util#install"]()
+  --   end,
+  -- },
 }
