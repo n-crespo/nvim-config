8a563a877a1390b20306ce9fcaad5ccd164578f8
Nicolas <nicolasecrespo@gmail.com>
2023-11-19 23:32:02 -0500

    removed autocmd to auto enable table mode on markdown files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,10 +6,10 @@ vim.api.nvim_create_autocmd({ "BufLeave", "BufWinLeave" }, {
   end,
 })
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-  callback = function(event)
-    if vim.bo[event.buf].filetype == "markdown" then
-      vim.api.nvim_command("TableModeEnable")
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+--   callback = function(event)
+--     if vim.bo[event.buf].filetype == "markdown" then
+--       vim.api.nvim_command("TableModeEnable")
+--     end
+--   end,
+-- })
