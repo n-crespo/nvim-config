8f0869c642ba598e16dc3dda438f153b12ced3e8
Nicolas <nicolasecrespo@gmail.com>
2023-11-24 15:49:56 -0500

    added filetype exclusion for scroll off plugin

--- init.lua
+++ init.lua
@@ -84,4 +84,14 @@ require("headlines").setup({
 })
 
 -- use better scroll-off plugin
-require("scrollEOF").setup()
+require("scrollEOF").setup({
+  -- The pattern used for the internal autocmd to determine
+  -- where to run scrollEOF. See https://neovim.io/doc/user/autocmd.html#autocmd-pattern
+  pattern = "*",
+  -- Whether or not scrollEOF should be enabled in insert mode
+  insert_mode = false,
+  -- List of filetypes to disable scrollEOF for.
+  disabled_filetypes = { "minifiles" },
+  -- List of modes to disable scrollEOF for. see https://neovim.io/doc/user/builtin.html#mode() for available modes.
+  disabled_modes = {},
+})
