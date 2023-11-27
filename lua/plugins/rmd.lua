4accabd94752729ecbc3099950c343db7b888c8c
nicolas <nicolasecrespo@gmail.com>
2023-11-27 13:32:59 -0500

    cleaning

--- lua/plugins/rmd.lua
+++ lua/plugins/rmd.lua
@@ -2,9 +2,8 @@ return {
   -- add functionality for rmarkdown
   {
     "vim-pandoc/vim-rmarkdown",
-    ft = { "rmd", "rmarkdown", "Rmd" },
     dependencies = {
-      { "vim-pandoc/vim-pandoc-syntax", ft = { "rmd", "rmarkdown", "Rmd" } },
+      { "vim-pandoc/vim-pandoc-syntax" },
     },
   },
 }
