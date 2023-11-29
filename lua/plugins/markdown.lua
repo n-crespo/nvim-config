25d77c712e1574d68a123031a133671d528b06fc
Nicolas Crespo <nicolasecrespo@gmail.com>
2023-11-29 17:00:49 -0500

    re-added markdown table mode because it is good

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,5 +1,6 @@
 return {
   { "ixru/nvim-markdown" },
+  { "dhruvasagar/vim-table-mode" },
   -- markdown preview is loaded through lazy extras (but it's having problems...)
   -- {
   --   "iamcco/markdown-preview.nvim",
