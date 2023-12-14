2ed3e945078b9aa6841a99779c7eaeda09ca6072
Nicolas <nicolasecrespo@gmail.com>
2023-12-13 21:48:31 -0500

    disabled headlines

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 require("config.lazy")
 
 -- for some reason my r markdown files have a rmd filetpe, this just allows the
--- headlines plugin to work for them too
+-- headlines plugin to work for them too (Disabled bc it makes things lag)
 -- require("headlines").setup({
 --   rmarkdown = require("headlines").config.rmd,
 -- })
