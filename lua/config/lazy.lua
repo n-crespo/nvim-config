1f46cb8a12967ae361c8a020b043e1fe9cea87d7
Nicolas <nicolasecrespo@gmail.com>
2023-12-13 21:47:44 -0500

    removed prettier formatter, not doing anything

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.lang.java" },
     -- { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "plugins" },
   },
