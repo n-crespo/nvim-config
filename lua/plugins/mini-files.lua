05aba115b8698860bf44704daf94ef55607741bf
Nicolas <nicolasecrespo@gmail.com>
2023-12-15 20:22:41 -0500

    disabled permanent delete, will now delete to ~/.local/share/nvim/minifiles

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -1,12 +1,11 @@
 return {
   "echasnovski/mini.files",
   event = "VimEnter",
+  version = "*",
   opts = {
     options = {
+      permanent_delete = false,
       use_as_default_explorer = true,
     },
   },
-  config = function()
-    vim.g.mini_files_use_netrw = 0 -- pls use mini files not netrw
-  end,
 }
