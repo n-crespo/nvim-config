69bb1d11484f433dc0bfbb96bab685360b4616bf
Nicolas <nicolasecrespo@gmail.com>
2023-12-19 00:22:23 -0500

    added keymaps to resize ranger plugin

--- lua/plugins/ranger.lua
+++ lua/plugins/ranger.lua
@@ -3,6 +3,8 @@ return {
   config = function()
     -- support for ranger plugin
     vim.keymap.set("n", "<leader>e", "<CMD>RnvimrToggle<CR>", { silent = true, desc = "[E]xplore" })
+    vim.keymap.set("t", "<leader>i", "<CMD>RnvimrResize<CR>", { silent = true, desc = "Resize Ranger" })
     vim.g.rnvimr_enable_ex = 1
+    vim.g.rnvimr_edit_cm = "$EDITOR"
   end,
 }
