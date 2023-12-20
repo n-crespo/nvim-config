92564b92bd0f5de45756b5ad2009ad2036a1bf6e
Nicolas <nicolasecrespo@gmail.com>
2023-12-19 23:00:43 -0500

    removed unneeded keymap

--- lua/plugins/ranger.lua
+++ lua/plugins/ranger.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     -- support for ranger plugin
     vim.keymap.set("n", "<leader>e", "<CMD>RnvimrToggle<CR>", { silent = true, desc = "[E]xplore" })
-    vim.keymap.set("t", "<leader>i", "<CMD>RnvimrResize<CR>", { silent = true, desc = "Resize Ranger" })
+    -- use <tab> to switch layouts (sizes)
     vim.g.rnvimr_enable_ex = 1
     vim.g.rnvimr_edit_cm = "$EDITOR"
   end,
