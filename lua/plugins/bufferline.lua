ac5da0a7b94966121c23e29fe867ecc49e540ad2
Nicolas <nicolasecrespo@gmail.com>
2023-12-19 22:58:01 -0500

    reduce minimium bufferline tab size

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,13 +2,13 @@ return {
   "akinsho/bufferline.nvim",
   opts = {
     highlights = {
-
       buffer_selected = {
         bold = true,
         italic = false,
       },
     },
     options = {
+      tab_size = 6,
       show_buffer_close_icons = false,
       show_close_icon = false,
     },
