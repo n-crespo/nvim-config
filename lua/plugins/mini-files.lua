7f32246d1e689eb4997eae08c87868076bc354e6
Nicolas <nicolasecrespo@gmail.com>
2023-12-27 20:22:58 -0500

    removed <leader>fm keymap in favor of <leader>e

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -26,13 +26,13 @@ return {
     },
   },
   keys = {
-    {
-      "<leader>fm",
-      function()
-        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-      end,
-      desc = "Open mini.files (directory of current file)",
-    },
+    -- {
+    --   "<leader>fm",
+    --   function()
+    --     require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+    --   end,
+    --   desc = "Open mini.files (directory of current file)",
+    -- },
     {
       "<leader>e",
       function()
