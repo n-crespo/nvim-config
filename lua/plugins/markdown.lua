cedc437ab206a3a0d299595dfebdc11fba401695
Nicolas <nicolasecrespo@gmail.com>
2023-12-30 18:30:51 -0500

    edited markdown plugin keymaps

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -5,9 +5,9 @@ return {
       -- markdown table of contents
       vim.keymap.set(
         "n",
-        "<leader>tc",
+        "<leader>mc",
         "<CMD>Toc<CR><CMD>set nornu<CR><cmd>set nonu<cr>",
-        { silent = true, desc = "TOC" }
+        { silent = true, desc = "[C]ontents" }
       )
       vim.g.vim_markdown_math = 1
       vim.g.vim_markdown_toc_autofit = 1
@@ -17,7 +17,7 @@ return {
     "dhruvasagar/vim-table-mode",
     config = function()
       -- support for markdown table mode
-      vim.keymap.set("n", "<leader>tm", "<CMD>TableModeToggle<CR>", { silent = true, desc = "Table [M]ode" })
+      vim.keymap.set("n", "<leader>mt", "<CMD>TableModeToggle<CR>", { silent = true, desc = "Table Mode [T]oggle" })
     end,
   },
   {
