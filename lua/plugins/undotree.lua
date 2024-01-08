9abfaeee06aa6655126ca0be8afa57887775e799
Nicolas <nicolasecrespo@gmail.com>
2024-01-07 21:57:40 -0500

    removed undo tree

--- lua/plugins/undotree.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- see a tree of recent changes and undos
-  "mbbill/undotree",
-  config = function()
-    vim.keymap.set("n", "<leader>ut", [[:UndotreeToggle <cr><c-w>h]], { silent = true, desc = "Undo [T]ree" })
-  end,
-}
