154442d35c25fa996bc2f3c381617e7f27053a0e
Nicolas <nicolasecrespo@gmail.com>
2024-01-11 01:54:07 -0500

    formatting

--- lua/plugins/minicursorword.lua
+++ lua/plugins/minicursorword.lua
@@ -1,12 +1,10 @@
 return {
-  {
-    "echasnovski/mini.cursorword",
-    version = false,
-    config = function()
-      require("mini.cursorword").setup({
-        -- Delay (in ms) between when cursor moved and when highlighting appeared
-        delay = 100,
-      })
-    end,
-  },
+  "echasnovski/mini.cursorword",
+  version = false,
+  config = function()
+    require("mini.cursorword").setup({
+      -- Delay (in ms) between when cursor moved and when highlighting appeared
+      delay = 100,
+    })
+  end,
 }
--- lua/plugins/minitabline.lua
+++ lua/plugins/minitabline.lua
@@ -1,20 +1,18 @@
 return {
-  {
-    "echasnovski/mini.tabline",
-    version = "*",
-    config = function()
-      require("mini.tabline").setup({
-        -- Whether to show file icons (requires 'nvim-tree/nvim-web-devicons')
-        show_icons = true,
+  "echasnovski/mini.tabline",
+  version = false,
+  config = function()
+    require("mini.tabline").setup({
+      -- Whether to show file icons (requires 'nvim-tree/nvim-web-devicons')
+      show_icons = true,
 
-        -- Whether to set Vim's settings for tabline (make it always shown and
-        -- allow hidden buffers)
-        set_vim_settings = true,
+      -- Whether to set Vim's settings for tabline (make it always shown and
+      -- allow hidden buffers)
+      set_vim_settings = true,
 
-        -- Where to show tabpage section in case of multiple vim tabpages.
-        -- One of 'left', 'right', 'none'.
-        tabpage_section = "right",
-      })
-    end,
-  },
+      -- Where to show tabpage section in case of multiple vim tabpages.
+      -- One of 'left', 'right', 'none'.
+      tabpage_section = "right",
+    })
+  end,
 }
