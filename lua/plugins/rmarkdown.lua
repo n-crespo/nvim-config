feeeea6739595a8ccc92d985f2236be3c8c16264
Nicolas <nicolasecrespo@gmail.com>
2024-01-12 11:41:55 -0500

    fixed r header keymap

--- lua/plugins/rmarkdown.lua
+++ lua/plugins/rmarkdown.lua
@@ -22,7 +22,7 @@ return {
     vim.keymap.set(
       "n",
       "<leader>rh",
-      [[i---<CR>title: ""<CR>author: ""<CR>date: "`r Sys.Date()`"<CR>output: html_document<CR>---<CR><CR>```{r setup, include=FALSE}<CR>knitr::opts_chunk$set(echo = TRUE)<CR>```<CR><CR>---<CR><CR>]],
+      [[i---<CR>title: ""<CR>author: "Nicolas Crespo"<CR>date: "`r Sys.Date()`"<CR>output: pdf_document<CR>---<CR><CR>```{r setup, include=FALSE}<CR>knitr::opts_chunk$set(echo = TRUE)<CR>```<CR><CR>---<CR><CR>]],
       { desc = "R Header" }
     )
 
