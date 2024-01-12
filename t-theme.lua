eb52fc2d7c581740d0f5aae90a17beb4549dd113
Nicolas <nicolasecrespo@gmail.com>
2024-01-12 02:44:25 -0500

    added lualine themeing files

---
+++ t-theme.lua
@@ -0,0 +1,53 @@
+local M = {}
+
+local pref = require("onedarkpro.helpers").get_colors("onedark_dark")
+M.theme = function()
+  local colors = {
+    -- normal = "#7e9cd8",
+    -- insert = "#98bb6c",
+    -- visual = "#ffa066",
+    -- replace = "#e46876",
+    darkgray = "#16161d",
+    gray = "#abb2bf",
+    innerbg = nil,
+    outerbg = "#16161D",
+    normal = pref.blue,
+    visual = pref.yellow,
+    insert = pref.green,
+    command = pref.purple,
+    replace = pref.red,
+  }
+  return {
+    inactive = {
+      a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    visual = {
+      a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    replace = {
+      a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    normal = {
+      a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    insert = {
+      a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    command = {
+      a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+  }
+end
+return M
