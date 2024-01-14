2f80274ed4064c6f2db65c09d30cb5bf4f0f4943
Nicolas <nicolasecrespo@gmail.com>
2024-01-13 21:27:16 -0500

    renamed themes and added default onedark theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
   -- 12 hour time because 'murica
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
-    opts.options.theme = require("transparent-lualine").theme
+    opts.options.theme = require("transparentlualine").theme
     opts.sections.lualine_z = {
       function()
         return " " .. os.date("%I:%M %p")
