3455502f3746c79096d64dde6fd57abc9b34fda3
Nicolas <nicolasecrespo@gmail.com>
2024-01-14 01:55:49 -0500

    lualine loading

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,7 @@ return {
   -- status bar at bottom of screen
   -- 12 hour time because 'murica
   "nvim-lualine/lualine.nvim",
+  event = { "BufReadPre", "BufNewFile" },
   opts = function(_, opts)
     opts.options.theme = require("transparentlualine").theme
     opts.sections.lualine_z = {
