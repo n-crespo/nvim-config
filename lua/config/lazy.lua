959bf0ccea07966f24fb641ba0a1e71c1b480b10
Nicolas <nicolasecrespo@gmail.com>
2024-01-13 20:18:21 -0500

    added comments

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,14 +14,17 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins
+    -- will load during startup. If you know what you're doing, you can set this
+    -- to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
+    -- It's recommended to leave version=false for now, since a lot the plugin
+    -- that support versioning have outdated releases, which may break your
+    -- Neovim install.
     version = false, -- always use the latest git commit
     -- version = "*",  try installing the latest stable version for plugins that support semver
   },
+  -- add borders and make :Lazy window smaller
   ui = {
     border = "rounded",
     size = {
@@ -29,6 +32,7 @@ require("lazy").setup({
       height = 0.8,
     },
   },
+  -- don't show message on config change/reload
   change_detection = {
     notify = false,
   },
