63c2a9566011846833428013067e960a4b76288f
Nicolas <nicolasecrespo@gmail.com>
2024-01-13 19:00:37 -0500

    added mason floating window ui borders and sizing

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,6 +2,22 @@ return {
   -- installing language servers
   "williamboman/mason.nvim",
   opts = {
+    ui = {
+      -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+      border = "rounded",
+
+      ---@since 1.0.0
+      -- Width of the window. Accepts:
+      -- - Integer greater than 1 for fixed width.
+      -- - Float in the range of 0-1 for a percentage of screen width.
+      width = 0.8,
+
+      ---@since 1.0.0
+      -- Height of the window. Accepts:
+      -- - Integer greater than 1 for fixed height.
+      -- - Float in the range of 0-1 for a percentage of screen height.
+      height = 0.8,
+    },
     ensure_installed = {
       "arduino-language-server",
       "bash-language-server",
