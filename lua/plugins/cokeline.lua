e955f4cce4ca1f11fab2cd2fa7ebde4ea2ee208c
Nicolas <nicolasecrespo@gmail.com>
2024-01-20 17:19:06 -0500

    added cokeline

---
+++ lua/plugins/cokeline.lua
@@ -0,0 +1,8 @@
+return {
+  "willothy/nvim-cokeline",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- Required for v0.4.0+
+    "nvim-tree/nvim-web-devicons", -- If you want devicons
+  },
+  config = true,
+}
