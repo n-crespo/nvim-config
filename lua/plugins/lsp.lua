924c604fd186ffa8e779c3e6560b0a30a0b7840e
Nicolas <nicolasecrespo@gmail.com>
2024-01-24 15:00:28 -0500

    added navbudddy config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,44 @@ return {
           "SmiteshP/nvim-navic",
           "MunifTanjim/nui.nvim",
         },
-        opts = { lsp = { auto_attach = true } },
+        opts = {
+          window = {
+            border = "rounded", -- "rounded", "double", "solid", "none"
+            -- or an array with eight chars building up the border in a clockwise fashion
+            -- starting with the top-left corner. eg: { "╔", "═" ,"╗", "║", "╝", "═", "╚", "║" }.
+            size = "60%", -- Or table format example: { height = "40%", width = "100%"}
+            position = "50%", -- Or table format example: { row = "100%", col = "0%"}
+            scrolloff = nil, -- scrolloff value within navbuddy window
+            sections = {
+              left = {
+                size = "20%",
+                border = nil, -- You can set border style for each section individually as well.
+              },
+              mid = {
+                size = "40%",
+                border = nil,
+              },
+              right = {
+                -- No size option for right most section. It fills to
+                -- remaining area.
+                border = nil,
+                preview = "leaf", -- Right section can show previews too.
+                -- Options: "leaf", "always" or "never"
+              },
+            },
+          },
+          node_markers = {
+            enabled = true,
+            icons = {
+              leaf = " ",
+              leaf_selected = " → ",
+              branch = "  ",
+            },
+          },
+          lsp = {
+            auto_attach = true,
+          },
+        },
         keys = {
           { "<leader>cs", "<cmd>Navbuddy<cr>", desc = "Navigate Code" },
         },
@@ -32,7 +69,7 @@ return {
         jdtls = {},
         jsonls = {},
         -- lua_ls = {},
-        -- marksman = {},
+        marksman = {},
         lua_ls = { enabled = false },
         r_language_server = {},
         ruff_lsp = {},
