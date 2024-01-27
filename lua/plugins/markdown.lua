876cd00aaac8a2a5d2805bfc3621c95a972c4930
Nicolas <nicolasecrespo@gmail.com>
2024-01-26 21:56:13 -0500

    cleaning

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,10 +1,8 @@
+-- Use <enter> to follow markdown links (or hyperlinks), <C-k> in insert mode
+-- to create links, <tab> to fold headers. Good. Kinda bugggy and not maintained
+-- but good
 vim.g.vim_markdown_toc_autofit = 1
 return {
-  -- use <enter> to follow markdown links (or hyperlinks), <C-k> in insert mode
-  -- to create links, <tab> to fold headers, <leader>mc for table of contents
   "ixru/nvim-markdown",
   ft = "markdown",
-  -- keys = {
-  --   -- { "<leader>t", "<cmd>Toc<cr><cmd>set nornu<cr><cmr>set nonu<cr>", desc = "Table of Contents" },
-  -- },
 }
