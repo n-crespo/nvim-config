5102b7b8d357bd2b7b2785de920921686802d3cf
Nicolas <nicolasecrespo@gmail.com>
2024-01-26 21:36:55 -0500

    no more require

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,10 +2,7 @@ return {
   -- below is needed when using transparent background
   -- default LazyVim plugin for notifications
   "rcarriga/nvim-notify",
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require("notify").setup({
-      background_colour = "#22272f",
-    })
-  end,
+  opts = {
+    background_colour = "#22272f",
+  },
 }
