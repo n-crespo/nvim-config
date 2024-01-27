55f6c474816270ab705a8f1ee5099a07b2519952
Nicolas <nicolasecrespo@gmail.com>
2024-01-26 21:38:22 -0500

    formatting

--- lua/plugins/flatten.lua
+++ lua/plugins/flatten.lua
@@ -3,7 +3,11 @@ return {
   -- in :term or betterterm, (C-q), using `nvim {file}` will open the file in
   -- the primary instance of neovim. amazing.
   "willothy/flatten.nvim",
-  opts = { window = { open = "alternate" } },
   lazy = false, -- Ensure that it runs first to minimize delay when opening file from terminal
   priority = 1001,
+  opts = {
+    window = {
+      open = "alternate",
+    },
+  },
 }
