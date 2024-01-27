f393ce4593afa6ad9c076db2c304c96fde4b8cf3
Nicolas <nicolasecrespo@gmail.com>
2024-01-26 21:49:10 -0500

    formatting and Do i even need Neoconf.nvim?

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,17 +1,17 @@
+-- lsp configurations
 return {
-  -- lsp configurations
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-    },
+    -- dependencies = {
+    --   { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+    -- },
     opts = {
       diagnostics = {
         float = {
           border = "rounded",
         },
       },
-      -- make sure mason installs the server
+      -- make sure mason installs these server
       servers = {
         pyright = {},
         clangd = {},
@@ -19,15 +19,14 @@ return {
         bashls = {},
         jdtls = {},
         jsonls = {},
-        lua_ls = { enabled = false },
-        marksman = { enabled = false },
-        lua_ls = { enabled = false },
+        lua_ls = { enabled = false }, -- i don't need this and it makes things laggy
+        marksman = { enabled = false }, -- i don't need this and it makes things laggy
         r_language_server = {},
         ruff_lsp = {},
       },
       setup = {
         jdtls = function()
-          return true -- avoid duplicate servers
+          return true -- avoid duplicate servers (??)
         end,
       },
     },
