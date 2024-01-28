50549f273df910e127413a1f4c6034f5f626b926
Nicolas <nicolasecrespo@gmail.com>
2024-01-28 16:07:47 -0500

    edit highlights

--- lua/plugins/onedarkpro.lua
+++ lua/plugins/onedarkpro.lua
@@ -32,9 +32,6 @@ return {
       TreeSitterContext = { bg = "#22272f" },
       MiniStarterItemBullet = { fg = "#89ca78" },
       TelescopeBorder = { fg = "#61afef" },
-      FloatBorder = { fg = "#61afef" },
-      VertSplit = { fg = "#61afef" },
-      -- WinSeparator = { fg = "#61afef" },
     },
   },
   config = function(_, opts)
