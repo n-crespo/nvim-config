f630089b9e8758d0b95f9a6cb2204fc2f70f08ab
Nicolas <nicolasecrespo@gmail.com>
2024-01-28 22:59:19 -0500

    formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,28 @@
 -- lsp configurations
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      diagnostics = {
-        float = {
-          border = "rounded",
-        },
+  "neovim/nvim-lspconfig",
+  opts = {
+    diagnostics = {
+      float = {
+        border = "rounded",
       },
-      servers = {
-        arduino_language_server = {},
-        pyright = {},
-        clangd = {},
-        bashls = {},
-        jdtls = {}, -- java
-        black = {},
-        cbfmt = {}, -- markdown code block
-        jsonls = {},
-        lua_ls = { enabled = false }, -- i don't need this and it makes things laggy
-        marksman = { enabled = false }, -- i don't need this and it makes things laggy
-        r_language_server = {},
-        ruff_lsp = {},
-      },
-      setup = {
-        jdtls = function()
-          return true -- avoid duplicate servers (??)
-        end,
-      },
+    },
+    servers = {
+      arduino_language_server = {},
+      pyright = {},
+      clangd = {},
+      bashls = {},
+      jdtls = {}, -- java
+      jsonls = {},
+      lua_ls = { enabled = false }, -- i don't need this and it makes things laggy
+      marksman = { enabled = false }, -- i don't need this and it makes things laggy
+      r_language_server = {},
+      ruff_lsp = {},
+    },
+    setup = {
+      jdtls = function()
+        return true -- avoid duplicate servers (??)
+      end,
     },
   },
 }
