b44efec51979cf2ec7c12f586453548ba7a2d691
Nicolas <nicolasecrespo@gmail.com>
2024-02-02 14:15:24 -0500

    cleaning

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,6 +1,6 @@
+-- below is needed when using transparent background
+-- (default LazyVim plugin for notifications)
 return {
-  -- below is needed when using transparent background
-  -- (default LazyVim plugin for notifications)
   "rcarriga/nvim-notify",
   opts = {
     background_colour = "#22272f",
