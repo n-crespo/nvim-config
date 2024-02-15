ab4f573a5de375f6af401a72e5798322f3c5b95a
Nicolas <nicolasecrespo@gmail.com>
2024-02-15 12:53:45 -0500

    added comments

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,13 +12,12 @@ return {
       pyright = {},
       clangd = {},
       bashls = {},
-      -- jdtls = { enabled = package.config:sub(1, 1) == ":" }, -- java
       jdtls = { enabled = true }, -- java
       jsonls = {},
       lua_ls = { enabled = false }, -- i don't need this and it makes things laggy
       marksman = { enabled = false }, -- i don't need this and it makes things laggy
       r_language_server = {},
-      ruff_lsp = {},
+      ruff_lsp = {}, -- for python
     },
     setup = {
       jdtls = function()
