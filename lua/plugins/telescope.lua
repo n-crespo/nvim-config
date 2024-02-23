df95e6fee018fd50be3f6c49697126ea8b1e5def
Nicolas <nicolasecrespo@gmail.com>
2024-02-23 09:55:34 -0500

    fixed telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,36 +5,36 @@
 local actions = require("telescope.actions")
 return {
   "telescope.nvim",
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    opts = {
-      initial_mode = "insert",
+  -- dependencies = {
+  --   "nvim-telescope/telescope-fzf-native.nvim",
+  --   build = "make",
+  --   opts = {
+  --     initial_mode = "insert",
+  --   },
+  --   extensions = {
+  --     fzf = {
+  --       fuzzy = true, -- false will only do exact matching
+  --       override_generic_sorter = true, -- override the generic sorter
+  --       override_file_sorter = true, -- override the file sorter
+  --       case_mode = "ignore_case", -- or "ignore_case" or "respect_case"
+  --       -- the default case_mode is "smart_case"
+  --     },
+  --   },
+  keys = {
+    {
+      "<leader>fp",
+      function()
+        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+      end,
+      desc = "Find Plugin File",
     },
-    extensions = {
-      fzf = {
-        fuzzy = true, -- false will only do exact matching
-        override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = "ignore_case", -- or "ignore_case" or "respect_case"
-        -- the default case_mode is "smart_case"
-      },
-    },
-    keys = {
-      {
-        "<leader>fp",
-        function()
-          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-        end,
-        desc = "Find Plugin File",
-      },
-      {
-        "<leader>fo",
-        "<cmd>Telescope oldfiles<cr>",
-        desc = "Find Old Files",
-      },
+    {
+      "<leader>fo",
+      "<cmd>Telescope oldfiles<cr>",
+      desc = "Find Old Files",
     },
   },
+  -- },
   opts = {
     defaults = {
       preview = {
@@ -51,6 +51,6 @@ return {
   },
   config = function(_, opts)
     require("telescope").setup(opts)
-    require("telescope").load_extension("fzf")
+    -- require("telescope").load_extension("fzf")
   end,
 }
