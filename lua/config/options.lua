8d219882f6f617283167f01be7d5f2fb3de5ced7
Nicolas <nicolasecrespo@gmail.com>
2024-02-29 14:18:10 -0500

    cole=2

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,7 +15,7 @@ local opt = vim.opt
 
 opt.clipboard = "" -- don't sync with system clipboard (see autocmds)
 opt.mouse = "" -- disable mouse
-opt.conceallevel = 3 -- Hide * markup for bold and italics
+opt.conceallevel = 2 -- Hide * markup for bold and italics
 opt.autowrite = true -- Enable auto writes
 opt.cursorline = false -- don't highlight current line (transparent background)
 opt.rnu = true -- relative line numbers
