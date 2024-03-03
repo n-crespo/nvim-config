bfc44756b14fdb66aa6567e4c705af9f13940f54
Nicolas <nicolasecrespo@gmail.com>
2024-03-03 16:54:58 -0500

    feat: markdown bullet concealing

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -1,29 +1,57 @@
-;; extends
+; extends
 ; (list
 ;    (list_item
 ;    (list_marker_minus) @conceal (#set! conceal "")
 ;     (task_list_marker_checked)
-;  )) 
+;  ))
 ;
 ;  (list
 ;   (list_item
 ;     (list_marker_minus) @conceal (#set! conceal "")
 ;     (task_list_marker_unchecked)
-;  )) 
+;  ))
 
- ([
+([
   (task_list_marker_checked)
- ] @markup.list.checked (#set! conceal "󰄲"))
+  ] @markup.list.checked (#set! conceal "󰄲"))
 
- ([
-   (task_list_marker_unchecked)
- ] @markup.list.unchecked (#set! conceal ""))
+([
+  (task_list_marker_unchecked)
+  ] @markup.list.unchecked (#set! conceal ""))
 
-((block_quote_marker)
- @markup.quote (#set! conceal "▏" ))
+; ((block_quote_marker)
+;  @markup.quote (#set! conceal "▏" ))
 
-((block_continuation)
- @markup.quote (#set! conceal "▏" ))
 
-; ((list_marker_dot)
-;  @conceal (#set! conceal "•"))
+; ((block_continuation) @markup.quote
+;                       (#set! "priority" 0)
+;                       (#set! conceal "▏" ))
+
+; ((list_marker_minus) @markup.list
+;                      (#set! conceal "◾" ))
+;                      ; (#set! conceal "᛫" ))
+;                      ; (#set! conceal "‣" ))
+;                      ; (#set! conceal "・" ))
+;                      (#set! conceal "•" ))
+;
+; ((list_marker_minus) @markup.list
+;                       (#set! conceal ""))
+
+
+; ([(list_marker_plus) (list_marker_star)]
+;   @markup.list
+;   (#offset! @markup.list 0 0 0 -1)
+;   (#set! priority 1000)
+;   (#set! conceal "•"))
+; ([(list_marker_plus) (list_marker_star)]
+;   @markup.list
+;   (#any-of? @markup.list "+" "*")
+;   (#set! conceal "•"))
+((list_marker_minus)
+  @markup.list
+  (#offset! @markup.list 0 0 0 -1)
+  (#set! conceal "•"))
+((list_marker_minus)
+  @markup.list
+  (#eq? @markup.list "-")
+  (#set! conceal "•"))
