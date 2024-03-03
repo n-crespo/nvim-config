890bc39994db0d597242fa72edb610b153485b2f
Nicolas <nicolasecrespo@gmail.com>
2024-03-03 00:47:45 -0500

    removed buggy markdown plugin, added markdowny

---
+++ lua/plugins/markdowny.lua
@@ -0,0 +1,14 @@
+return {
+  "antonk52/markdowny.nvim",
+  ft = "markdown",
+  config = function()
+    require("markdowny").setup({ filetypes = { "markdown", "txt" } })
+    vim.keymap.set("v", "<C-k>", "<cmd>lua require('markdowny').link()<cr>gv", { silent = true, buffer = 0 })
+    vim.keymap.set("v", "<C-b>", "<cmd>lua require('markdowny').bold()<cr>gv", { silent = true, buffer = 0 })
+    vim.keymap.set("n", "<C-b>", "V<C-b>", { remap = true, silent = true, buffer = 0 })
+    vim.keymap.set("v", "<C-i>", "<cmd>lua require('markdowny').italic()<cr>gv", { silent = true, buffer = 0 })
+    vim.keymap.set("n", "<C-i>", "V<C-i>", { remap = true, silent = true, buffer = 0 })
+    vim.keymap.set("v", "<C-e>", "<cmd>lua require('markdowny').code()<cr>gv", { buffer = 0 })
+    vim.keymap.set("n", "<C-e>", "v$<C-e>gv", { remap = true, silent = true, buffer = 0 })
+  end,
+}
