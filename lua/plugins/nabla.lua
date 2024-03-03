63385e9d937a393f9df90f71325805b22d142674
Nicolas <nicolasecrespo@gmail.com>
2024-03-02 23:28:58 -0500

    added nabla?

---
+++ lua/plugins/nabla.lua
@@ -0,0 +1,14 @@
+return {
+  "jbyuki/nabla.nvim",
+  lazy = true,
+  ft = "markdown",
+  keys = {
+    { "<leader>cp", "<cmd>lua require('nabla').popup({ border='rounded' })<cr>", desc = "Preview Equation" },
+  },
+  config = function()
+    require("nabla").enable_virt({
+      autogen = true,
+      silent = true,
+    })
+  end,
+}
