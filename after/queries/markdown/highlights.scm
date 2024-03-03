aecf2611bee9304b1946cde1b87feafd4931d754
Nicolas <nicolasecrespo@gmail.com>
2024-03-03 17:01:14 -0500

    docs: added comments and formatting

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -1,16 +1,8 @@
-; extends
-; (list
-;    (list_item
-;    (list_marker_minus) @conceal (#set! conceal "")
-;     (task_list_marker_checked)
-;  ))
-;
-;  (list
-;   (list_item
-;     (list_marker_minus) @conceal (#set! conceal "")
-;     (task_list_marker_unchecked)
-;  ))
+;extends
 
+; this breaks without that ^ (why? who knows)
+
+; replace checboxes with nice looking boxes
 ([
   (task_list_marker_checked)
   ] @markup.list.checked (#set! conceal "󰄲"))
@@ -19,39 +11,41 @@
   (task_list_marker_unchecked)
   ] @markup.list.unchecked (#set! conceal ""))
 
-; ((block_quote_marker)
-;  @markup.quote (#set! conceal "▏" ))
+; this works but only for the first line of the block quote
+((block_quote_marker)
+ @markup.quote (#set! conceal "▏" ))
 
 
+; BUG: this breaks things (will hide indented/nested bullets)
 ; ((block_continuation) @markup.quote
 ;                       (#set! "priority" 0)
 ;                       (#set! conceal "▏" ))
 
-; ((list_marker_minus) @markup.list
-;                      (#set! conceal "◾" ))
-;                      ; (#set! conceal "᛫" ))
-;                      ; (#set! conceal "‣" ))
-;                      ; (#set! conceal "・" ))
-;                      (#set! conceal "•" ))
+
+;ALTERNATIVE CHARACTERS
+;(#set! conceal "◾" ))
+;; (#set! conceal "᛫" ))
+;; (#set! conceal "‣" ))
+;; (#set! conceal "・" ))
+;(#set! conceal "•" ))
 ;
-; ((list_marker_minus) @markup.list
-;                       (#set! conceal ""))
 
-
-; ([(list_marker_plus) (list_marker_star)]
-;   @markup.list
-;   (#offset! @markup.list 0 0 0 -1)
-;   (#set! priority 1000)
-;   (#set! conceal "•"))
-; ([(list_marker_plus) (list_marker_star)]
-;   @markup.list
-;   (#any-of? @markup.list "+" "*")
-;   (#set! conceal "•"))
+; if you want rounded bullet: replace conceal char with • and uncomment offset
+; lines
+([(list_marker_plus) (list_marker_star)]
+  @markup.list
+  ; (#offset! @markup.list 0 0 0 -1)
+  (#set! priority 1000)
+  (#set! conceal "◾"))
+([(list_marker_plus) (list_marker_star)]
+  @markup.list
+  (#any-of? @markup.list "+" "*")
+  (#set! conceal "◾"))
 ((list_marker_minus)
   @markup.list
-  (#offset! @markup.list 0 0 0 -1)
-  (#set! conceal "•"))
+  ; (#offset! @markup.list 0 0 0 -1)
+  (#set! conceal "◾"))
 ((list_marker_minus)
   @markup.list
   (#eq? @markup.list "-")
-  (#set! conceal "•"))
+  (#set! conceal "◾"))
