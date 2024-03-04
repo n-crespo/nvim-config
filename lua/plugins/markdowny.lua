3fdbbecc0930502336d49dbb1d3125f4915baa0f
Nicolas <nicolasecrespo@gmail.com>
2024-03-03 23:24:39 -0500

    cleaning

--- lua/plugins/markdowny.lua
+++ lua/plugins/markdowny.lua
@@ -2,9 +2,6 @@ return {
   "n-crespo/markdowny.nvim",
   lazy = true,
   ft = "markdown",
-  -- config = function()
-  -- require("markdowny").setup({ filetypes = { "markdown", "txt" } })
-  -- end,
   keys = {
     { mode = "v", "<C-k>", "<cmd>lua require('markdowny').link()<cr>", desc = "Markdowny link" },
     { mode = "v", "<C-b>", "<cmd>lua require('markdowny').bold()<cr>", desc = "Markdowny bold" },
