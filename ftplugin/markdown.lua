8c89266e88ca6b5557eda8fdf69efc5710ec5374
Nicolas <nicolasecrespo@gmail.com>
2024-03-05 11:59:58 -0500

    disabled nabla, added markdown math keymap

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -10,3 +10,10 @@ vim.keymap.set("i", "$", "$$<left>", { buffer = true, silent = true })
 vim.keymap.set("i", "_", "__<left>", { buffer = true, silent = true })
 vim.keymap.set("i", "<C-e>", "```<cr><cr>```<up><up>", { buffer = true, silent = true })
 vim.cmd([[ setlocal commentstring=<!--\ %s\ --> ]])
+vim.keymap.set("n", "<leader>m", function()
+  vim.cmd([[
+  ownsyntax on
+  set spelllang=en
+  set spell
+]])
+end, { desc = "Preview Math", silent = true, buffer = true })
--- lua/plugins/nabla.lua
+++ lua/plugins/nabla.lua
@@ -1,22 +1,17 @@
 -- this is kinda buggy
 return {
   "jbyuki/nabla.nvim",
-  enabled = true, --- this is so unreliable, rather just use mkp
+  enabled = false, --- this is so unreliable, rather just use mkp
   lazy = true,
   ft = "markdown",
   keys = {
     { "<leader>p", "<cmd>lua require('nabla').popup({ border='rounded' })<cr>", desc = "Preview Equation" },
   },
   -- config = function()
-  --   -- require("nabla").enable_virt({
-  --   --   autogen = true,
-  --   --   silent = false,
-  --   --   align_center = true,
-  --   vim.keymap.set(
-  --     "n",
-  --     "<leader>p",
-  --     "<cmd>lua require('nabla').popup({ border='rounded' })<cr>",
-  --     { desc = "Preview Equation" }
-  --   )
+  --   require("nabla").enable_virt({
+  --     autogen = true,
+  --     silent = false,
+  --     align_center = true,
+  --   })
   -- end,
 }
