52903f9bb835f0880a4a0e80c03fbcd4ca47ee29
Nicolas <nicolasecrespo@gmail.com>
2024-03-06 18:55:26 -0500

    added enfocado

---
+++ lua/plugins/enfocado.lua
@@ -0,0 +1,22 @@
+return {
+  "wuelnerdotexe/vim-enfocado",
+  lazy = false,
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "enfocado",
+      },
+      sections = {
+        lualine_z = {
+          function()
+            return " " .. os.date("%I:%M %p")
+          end,
+        },
+      },
+    })
+    vim.cmd([[
+    let g:enfocado_style = 'nature' " Available: `nature` or `neon`.
+    ]])
+    vim.cmd([[colorscheme enfocado]])
+  end,
+}
