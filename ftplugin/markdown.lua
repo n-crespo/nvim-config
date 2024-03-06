de609e01e364dc1e2183dfffe21d2d2d10c25258
Nicolas <nicolasecrespo@gmail.com>
2024-03-06 14:10:59 -0500

    switched custom save command for autocmd

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,12 +1,17 @@
-vim.keymap.set(
-  "n",
-  "<C-s>",
-  "<cmd>TableModeRealign<cr><cmd>w<cr>",
-  { desc = "Save and Format File", buffer = true, silent = true }
-)
 vim.keymap.set("i", "<S-Tab>", "<C-d>", { buffer = true, silent = true })
 vim.keymap.set("i", "*", "**<left>", { buffer = true, silent = true })
 vim.keymap.set("i", "$", "$$<left>", { buffer = true, silent = true })
 vim.keymap.set("i", "_", "__<left>", { buffer = true, silent = true })
 vim.keymap.set("i", "<C-e>", "```<cr><cr>```<up><up>", { buffer = true, silent = true })
 vim.cmd([[ setlocal commentstring=<!--\ %s\ --> ]])
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*.md" },
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    pcall(function()
+      vim.cmd([[TableModeRealign]])
+    end)
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
