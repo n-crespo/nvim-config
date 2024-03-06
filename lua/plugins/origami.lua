a2e808007e09803bd73887393ae079c25d125a3a
Nicolas <nicolasecrespo@gmail.com>
2024-03-06 18:55:20 -0500

    set foldmethod to indent when origami loads

--- lua/plugins/origami.lua
+++ lua/plugins/origami.lua
@@ -3,4 +3,7 @@ return {
   "chrisgrieser/nvim-origami",
   event = "BufReadPost", -- later or on keypress would prevent saving folds
   opts = true, -- needed even when using default config
+  setup = function()
+    vim.cmd([[set fdm=indent]])
+  end,
 }
