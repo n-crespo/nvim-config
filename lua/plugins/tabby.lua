520c170a1159da4394f166275be58992ce0194c5
Nicolas <nicolasecrespo@gmail.com>
2024-03-06 18:55:07 -0500

    fixed tabby config

--- lua/plugins/tabby.lua
+++ lua/plugins/tabby.lua
@@ -38,12 +38,13 @@ return {
         line.spacer(),
         -- shows list of windows in tab
         line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+          local hl = win.is_current() and theme.current_tab or theme.tab
           return {
-            line.sep("", win.is_current() and theme.current_tab or theme.win, theme.fill),
+            line.sep("", hl, theme.fill),
             win.is_current() and "" or "",
             win.buf_name(),
-            line.sep("", win.is_current() and theme.current_tab or theme.win, theme.fill),
-            hl = win.is_current() and theme.current_tab or theme.win,
+            line.sep("", hl, theme.fill),
+            hl = hl,
             margin = " ",
           }
         end),
@@ -56,7 +57,6 @@ return {
     end)
   end,
   keys = {
-
     {
       "<leader>n",
       ":TabRename ",
