49913e6015ef2a29269634531c281a090a3f2256
Nicolas <nicolasecrespo@gmail.com>
2024-03-06 20:41:05 -0500

    cleaning

--- lua/plugins/enfocado.lua
+++ lua/plugins/enfocado.lua
@@ -1,24 +1,16 @@
 return {
-  {
-    "wuelnerdotexe/vim-enfocado",
-    lazy = false,
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "enfocado",
-        },
-        sections = {
-          lualine_z = {
-            function()
-              return " " .. os.date("%I:%M %p")
-            end,
-          },
-        },
-      })
-      vim.cmd([[
-    let g:enfocado_style = 'nature' " Available: `nature` or `neon`.
-    ]])
-      vim.cmd([[colorscheme enfocado]])
-    end,
-  },
+  "wuelnerdotexe/vim-enfocado",
+  lazy = false,
+  config = function()
+    require("lualine").setup({
+      options = { theme = "enfocado" },
+      sections = { lualine_z = {
+        function()
+          return "" .. os.date("%I:%M %p")
+        end,
+      } },
+    })
+    vim.cmd([[let g:enfocado_style = 'nature' " Available: `nature` or `neon`.]])
+    vim.cmd([[colorscheme enfocado]])
+  end,
 }
--- lua/plugins/tsactions.lua
+++ lua/plugins/tsactions.lua
@@ -6,14 +6,11 @@ return {
   config = function()
     require("ts-node-action").setup({})
     vim.keymap.set("n", "m", function()
-      local save_cursor = vim.fn.getpos(".")
+      localsave_cursor = vim.fn.getpos(".")
       pcall(function()
         require("ts-node-action").node_action()
       end)
       vim.fn.setpos(".", save_cursor)
-    end, {
-      desc = "Node Action",
-    })
-    vim.cmd([[echo 'keymap set']])
+    end, { desc = "Node Action" })
   end,
 }
