0831a4e93ea9462cf251f610d876acb9854b3a29
Nicolas <nicolasecrespo@gmail.com>
2024-03-08 11:20:26 -0500

    trying to fix faster

--- lua/plugins/faster.lua
+++ lua/plugins/faster.lua
@@ -20,13 +20,14 @@ return {
           "matchparen",
           "lsp",
           "treesitter",
+          "lualine",
           "indent_blankline",
           "vimopts",
           "syntax",
           "filetype",
         },
         -- Files larger than `filesize` are considered big files. Value is in MB.
-        filesize = 3,
+        filesize = 0.5,
         -- Autocmd pattern that controls on which files behaviour will be applied.
         -- `*` means any file.
         pattern = "*",
@@ -53,61 +54,55 @@ return {
     -- Defined feature will be used by faster.nvim only if it is on (`on=true`).
     -- Defer will be used if some features need to be disabled after others.
     -- defer=false features will be disabled first and defer=true features last.
-    features = {
-      -- Neovim filetype plugin
-      -- https://neovim.io/doc/user/filetype.html
-      filetype = {
-        on = true,
-        defer = true,
-      },
-      -- Illuminate plugin
-      -- https://github.com/RRethy/vim-illuminate
-      illuminate = {
-        on = true,
-        defer = false,
-      },
-      -- Indent Blankline
-      -- https://github.com/lukas-reineke/indent-blankline.nvim
-      indent_blankline = {
-        on = true,
-        defer = false,
-      },
-      -- Neovim LSP
-      -- https://neovim.io/doc/user/lsp.html
-      lsp = {
-        on = true,
-        defer = false,
-      },
-      -- Lualine
-      -- https://github.com/nvim-lualine/lualine.nvim
-      lualine = {
-        on = true,
-        defer = false,
-      },
-      -- Neovim Pi_paren plugin
-      -- https://neovim.io/doc/user/pi_paren.html
-      matchparen = {
-        on = true,
-        defer = false,
-      },
-      -- Neovim syntax
-      -- https://neovim.io/doc/user/syntax.html
-      syntax = {
-        on = true,
-        defer = true,
-      },
-      -- Neovim treesitter
-      -- https://neovim.io/doc/user/treesitter.html
-      treesitter = {
-        on = true,
-        defer = false,
-      },
-      -- Neovim options that affect speed when big file is opened:
-      -- swapfile, foldmethod, undolevels, undoreload, list
-      vimopts = {
-        on = true,
-        defer = false,
-      },
-    },
+    -- features = {
+    --   -- Neovim filetype plugin
+    --   -- https://neovim.io/doc/user/filetype.html
+    --   filetype = {
+    --     on = false,
+    --     defer = true,
+    --   },
+    --   -- Indent Blankline
+    --   -- https://github.com/lukas-reineke/indent-blankline.nvim
+    --   indent_blankline = {
+    --     on = false,
+    --     defer = false,
+    --   },
+    --   -- Neovim LSP
+    --   -- https://neovim.io/doc/user/lsp.html
+    --   lsp = {
+    --     on = false,
+    --     defer = false,
+    --   },
+    --   -- Lualine
+    --   -- https://github.com/nvim-lualine/lualine.nvim
+    --   lualine = {
+    --     on = false,
+    --     defer = false,
+    --   },
+    --   -- Neovim Pi_paren plugin
+    --   -- https://neovim.io/doc/user/pi_paren.html
+    --   matchparen = {
+    --     on = false,
+    --     defer = false,
+    --   },
+    --   -- Neovim syntax
+    --   -- https://neovim.io/doc/user/syntax.html
+    --   syntax = {
+    --     on = false,
+    --     defer = true,
+    --   },
+    --   -- Neovim treesitter
+    --   -- https://neovim.io/doc/user/treesitter.html
+    --   treesitter = {
+    --     on = false,
+    --     defer = false,
+    --   },
+    --   -- Neovim options that affect speed when big file is opened:
+    --   -- swapfile, foldmethod, undolevels, undoreload, list
+    --   vimopts = {
+    --     on = true,
+    --     defer = false,
+    --   },
+    -- },
   },
 }
