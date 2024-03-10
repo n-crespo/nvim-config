454c0e3e2aecbbd82a8f1ada5aa47dc06627d614
Nicolas <nicolasecrespo@gmail.com>
2024-03-10 15:58:05 -0400

    removed markdowny, update ss

--- ftplugin/rmarkdown.lua
+++ ftplugin/rmarkdown.lua
@@ -6,5 +6,5 @@ vim.keymap.set(
   "n",
   "<leader>rh",
   'i---<CR>title: ""<CR>author: "Nicolas Crespo"<CR>date: "`r Sys.Date()`"<CR>output: pdf_document<CR>---<CR><CR>```{r setup, include=FALSE}<CR>knitr::opts_chunk$set(echo = TRUE)<CR>```<CR><CR>---<CR><CR>',
-  { desc = "R Code Block" }
+  { desc = "Rmarkdown Header" }
 )
