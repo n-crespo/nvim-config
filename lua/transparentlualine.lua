fa82081359421a20a58d79ac872e343484cc7d6e
Nicolas <nicolasecrespo@gmail.com>
2024-03-10 23:08:05 -0400

    lualine colors

--- lua/transparentlualine.lua
+++ lua/transparentlualine.lua
@@ -17,8 +17,8 @@ M.theme = function()
     command = dgray,
     innerbg = nil,
     outerbg = "#141414",
-    darkgray = "#f2e9de",
-    gray = "#f2e9de",
+    darkgray = "#abb2bf",
+    gray = "#abb2bf",
   }
 
   return {
