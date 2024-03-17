631094cec40ef3520ea0c5aca4a479864b39756e
Nicolas <nicolasecrespo@gmail.com>
2024-03-17 18:39:06 -0400

    fixed lazy bootstrapping code

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -2,7 +2,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
    -- bootstrap lazy.nvim
    -- stylua: ignore
-  vim.fn.system({ "git", "clone", "filter=blob:none", "https://github.com/folke/lazy.nvim.git", "branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -27,6 +27,7 @@ require("lazy").setup({
   change_detection = {
     notify = false,
   },
+  install = { colorscheme = { "tokyonight", "habamax" } },
   -- install = { colorscheme = { "onedark_dark" } },
   checker = { enabled = true }, --   automatically check for plugin updates
   performance = {
@@ -39,28 +40,28 @@ require("lazy").setup({
         "tarPlugin",
         "tutor",
         "zipPlugin",
-        "netrw",
-        "netrwSettings",
-        "netrwFileHandlers",
-        "zip",
-        "tar",
-        "tarPlugin",
-        "getscript",
-        "getscriptPlugin",
-        "vimball",
-        "vimballPlugin",
-        "2html_plugin",
-        "logipat",
-        "rrhelper",
-        "spellfile_plugin",
-        "tohtml",
-        "tutor",
-        "rplugin",
-        "synmenu",
-        "optwin",
-        "compiler",
-        "bugreport",
-        "ftplugin",
+        -- "netrw",
+        -- "netrwSettings",
+        -- "netrwFileHandlers",
+        -- "zip",
+        -- "tar",
+        -- "tarPlugin",
+        -- "getscript",
+        -- "getscriptPlugin",
+        -- "vimball",
+        -- "vimballPlugin",
+        -- "2html_plugin",
+        -- "logipat",
+        -- "rrhelper",
+        -- "spellfile_plugin",
+        -- "tohtml",
+        -- "tutor",
+        -- "rplugin",
+        -- "synmenu",
+        -- "optwin",
+        -- "compiler",
+        -- "bugreport",
+        -- "ftplugin",
       },
     },
   },
