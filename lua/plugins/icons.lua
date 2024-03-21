267ea74e817774df2e9830f6cbcb93c153d53267
Nicolas <nicolasecrespo@gmail.com>
2024-03-21 11:53:41 -0400

    added correct icons for r markdown

---
+++ lua/plugins/icons.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-tree/nvim-web-devicons",
+  opts = {
+    override_by_extension = {
+      ["Rmd"] = {
+        icon = "",
+        color = "#519aba",
+        name = "Rmd",
+      },
+    },
+  },
+}
