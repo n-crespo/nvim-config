34eb117df739951d9021f676becad894f919b793
Nicolas <nicolasecrespo@gmail.com>
2024-03-21 11:37:39 -0400

    tabby changes, don't spell in rmarkdown, close terminal after flatten, spell

--- lua/plugins/flatten.lua
+++ lua/plugins/flatten.lua
@@ -5,6 +5,19 @@ return {
   "willothy/flatten.nvim",
   lazy = false, -- Ensure that it runs first to minimize delay when opening file from terminal
   opts = {
+    callbacks = {
+      ---Called before a nested session is opened.
+      pre_open = function()
+        require("betterTerm").open()
+      end,
+      -- ---Called after a nested session is opened.
+      -- ---@param bufnr buffer
+      -- ---@param winnr window
+      -- ---@param filetype string
+      -- ---@param is_blocking boolean
+      -- ---@param is_diff boolean
+      -- post_open = function(bufnr, winnr, filetype, is_blocking, is_diff) end,
+    },
     window = {
       open = "tab",
     },
