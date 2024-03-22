fb6b4f70bc78030b3cd86dc7ede606313518d98c
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-03-21 21:43:52 -0400

    codeum

---
+++ lua/plugins/codeium.lua
@@ -0,0 +1,38 @@
+return {
+
+  -- codeium cmp source
+  {
+    "nvim-cmp",
+    dependencies = {
+      -- codeium
+      {
+        "Exafunction/codeium.nvim",
+        cmd = "Codeium",
+        build = ":Codeium Auth",
+        opts = {
+          enable_chat = true,
+        },
+        config = function(_, opts)
+          require("codeium").setup(opts)
+        end,
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "codeium",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, 2, require("lazyvim.util").lualine.cmp_source("codeium"))
+    end,
+  },
+}
