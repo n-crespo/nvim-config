aff86b5494a3e22bdec606db2d2f52264e55a392
Nicolas <nicolasecrespo@gmail.com>
2024-03-28 19:20:49 -0500

    add incline, remove windows from tabby, TSContext bg higlight

--- lua/plugins/tabby.lua
+++ lua/plugins/tabby.lua
@@ -65,27 +65,27 @@ return {
         end),
         line.spacer(),
         -- shows list of windows in tab
-        line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-          local hl = win.is_current() and theme.current_tab or theme.transparent
-          local sephl = win.is_current() and theme.focused or theme.transparent
-          local modified = win_is_modified(win) and "" or ""
-          local sepright = win.is_current() and line.sep("", hl, sephl) or " "
-          local sepleft = win.is_current() and line.sep("", hl, sephl) or " "
-          return {
-            sepleft,
-            win.file_icon(),
-            -- win.is_current() and "" or "",
-            win.buf_name(),
-            modified,
-            sepright,
-            hl = hl,
-            margin = " ",
-          }
-        end),
-        {
-          line.sep("", theme.tail, theme.fill),
-          { "  ", hl = theme.tail },
-        },
+        -- line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
+        --   local hl = win.is_current() and theme.current_tab or theme.transparent
+        --   local sephl = win.is_current() and theme.focused or theme.transparent
+        --   local modified = win_is_modified(win) and "" or ""
+        --   local sepright = win.is_current() and line.sep("", hl, sephl) or " "
+        --   local sepleft = win.is_current() and line.sep("", hl, sephl) or " "
+        --   return {
+        --     sepleft,
+        --     win.file_icon(),
+        --     -- win.is_current() and "" or "",
+        --     win.buf_name(),
+        --     modified,
+        --     sepright,
+        --     hl = hl,
+        --     margin = " ",
+        --   }
+        -- end),
+        -- {
+        --   line.sep("", theme.tail, theme.fill),
+        --   { "  ", hl = theme.tail },
+        -- },
         hl = theme.fill,
       }
     end)
