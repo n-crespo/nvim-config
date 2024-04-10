749d57567f258210d60727dc829d3704584b367c
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-04-10 17:52:20 -0400

    moved options to correct places

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -9,6 +9,7 @@ return {
   --   { "<leader>t", "<cmd>Toc<cr><cmd>set nornu<cr><cmd>set nonu<cr>", desc = "Table of Contents" },
   -- },
   config = function()
+    vim.g.vim_markdown_toc_autofit = 1
     vim.g.vim_markdown_math = 1
     vim.cmd([[map zh <Plug>Markdown_Fold]])
     vim.cmd([[map <Plug> <Plug>Markdown_CreateLink]])
