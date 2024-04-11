bbbf41e7a5b8a22e248c6ce13d2a0031fb1c9488
Nicolas <nicolasecrespo@gmail.com>
2024-04-10 20:46:54 -0500

    square nabla popup corners

--- lua/plugins/nabla.lua
+++ lua/plugins/nabla.lua
@@ -4,7 +4,7 @@ return {
   enabled = true, --- this is so unreliable, rather just use mkp
   ft = "markdown",
   keys = {
-    { "<leader>p", "<cmd>lua require('nabla').popup({ border='rounded' })<cr>", desc = "Preview Equation" },
+    { "<leader>p", "<cmd>lua require('nabla').popup({ border='single' })<cr>", desc = "Preview Equation" },
   },
   config = function()
     require("nabla").enable_virt({
