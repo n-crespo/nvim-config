92cc5698a138e6aad0f19c5bfdbcb696b3ccfca2
Nicolas <nicolasecrespo@gmail.com>
2024-04-12 12:39:06 -0400

    buffer local toc keymap

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -5,7 +5,7 @@ vim.keymap.set("i", "_", "__<left>", { buffer = true, silent = true })
 vim.keymap.set("i", "<C-e>", "```<cr><cr>```<up><up>", { buffer = true, silent = true })
 vim.cmd([[ setlocal commentstring=<!--\ %s\ --> ]])
 
-vim.keymap.set("n", "<leader>cs", "<cmd>Toc<cr><cmd>set nonu nornu<cr>")
+vim.keymap.set("n", "<leader>cs", "<cmd>Toc<cr><cmd>set nonu nornu<cr>", { buffer = true, silent = true })
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*.md" },
