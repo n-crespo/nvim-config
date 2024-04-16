03b0d962a96339901b4704547babaed0925ddaf5
Nicolas <nicolasecrespo@gmail.com>
2024-04-15 20:58:10 -0400

    add custom html buffer specific keymaps for opening localhost

---
+++ after/ftplugin/html.lua
@@ -0,0 +1,13 @@
+vim.keymap.set(
+  "n",
+  "<leader>ow",
+  "<cmd>open http://localhost:8000/<cr>",
+  { desc = "Open Server", buffer = true, silent = true }
+)
+
+vim.keymap.set(
+  "n",
+  "<leader>os",
+  "<cmd>open http://localhost:8000/<cr>",
+  { desc = "Open Server", buffer = true, silent = true }
+)
