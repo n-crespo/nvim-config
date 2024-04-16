58156394fe2a974a8d6c38ea0f14cae682cab5bf
Nicolas <nicolasecrespo@gmail.com>
2024-04-15 21:24:26 -0400

    fix cmds

--- after/ftplugin/html.lua
+++ after/ftplugin/html.lua
@@ -1,13 +1,13 @@
 vim.keymap.set(
   "n",
   "<leader>ow",
-  "<cmd>!open localhost:3000<cr>",
+  "<cmd>!open http://localhost:3000/<cr>",
   { desc = "Open Server", buffer = true, silent = true }
 )
 
 vim.keymap.set(
   "n",
   "<leader>os",
-  "<cmd>!open localhost:3000<cr>",
+  "<cmd>!open https://localhost:3000<cr>",
   { desc = "Open Server", buffer = true, silent = true }
 )
