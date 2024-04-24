4dd172f1040154f7e6a379b10a20210517ba8e5d
Nicolas <nicolasecrespo@gmail.com>
2024-04-24 04:03:05 -0400

    fixed lualine theme

--- lua/transparentlualine.lua
+++ lua/transparentlualine.lua
@@ -1,49 +1,49 @@
-local dgray = "#0d0c0c"
+local azbg = "#201d1d"
 
 local M = {}
 
 M.theme = function()
   local colors = {
-    normal = dgray,
-    visual = dgray,
-    replace = dgray,
-    insert = dgray,
-    command = dgray,
+    normal = azbg,
+    visual = azbg,
+    replace = azbg,
+    insert = azbg,
+    command = azbg,
     innerbg = nil,
-    outerbg = "#201d1d",
-    darkgray = "#abb2bf",
-    gray = "#abb2bf",
+    bybg = "#0d0c0c",
+    darkgray = "#b4b3a7",
+    gray = "#b4b3a7",
   }
 
   return {
     inactive = {
-      a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
+      a = { fg = colors.gray, bg = colors.bybg, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.bybg },
       c = { fg = colors.gray, bg = colors.innerbg },
     },
     visual = {
       a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
+      b = { fg = colors.gray, bg = colors.bybg },
       c = { fg = colors.gray, bg = colors.innerbg },
     },
     replace = {
       a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
+      b = { fg = colors.gray, bg = colors.bybg },
       c = { fg = colors.gray, bg = colors.innerbg },
     },
     normal = {
       a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
+      b = { fg = colors.gray, bg = colors.bybg },
       c = { fg = colors.gray, bg = colors.innerbg },
     },
     insert = {
       a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
+      b = { fg = colors.gray, bg = colors.bybg },
       c = { fg = colors.gray, bg = colors.innerbg },
     },
     command = {
       a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
-      b = { fg = colors.gray, bg = colors.outerbg },
+      b = { fg = colors.gray, bg = colors.bybg },
       c = { fg = colors.gray, bg = colors.innerbg },
     },
   }
