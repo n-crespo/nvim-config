32467118da89543b7b670fca59a2f5686a844749
Nicolas <nicolasecrespo@gmail.com>
2024-04-26 23:57:56 -0400

    cleaned up incline config

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -12,23 +12,6 @@ return {
         end
 
         local ft_icon, ft_color = devicons.get_icon_color(filename)
-        local function get_git_diff()
-          local icons = { removed = " ", changed = " ", added = " " }
-          local signs = vim.b[props.buf].gitsigns_status_dict
-          local labels = {}
-          if signs == nil then
-            return labels
-          end
-          for name, icon in pairs(icons) do
-            if tonumber(signs[name]) and signs[name] > 0 then
-              table.insert(labels, { icon .. signs[name] .. " ", group = "Diff" .. name })
-            end
-          end
-          if #labels > 0 then
-            table.insert(labels, { "│ " })
-          end
-          return labels
-        end
 
         local function get_diagnostic_label()
           local icons = { error = " ", warn = " ", info = " ", hint = " " }
@@ -49,10 +32,8 @@ return {
         local modified = vim.bo[props.buf].modified and "  " or ""
         return {
           { get_diagnostic_label() },
-          -- { get_git_diff() },
           { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
           { filename .. "" .. modified },
-          -- { "┊  " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
         }
       end,
     })
