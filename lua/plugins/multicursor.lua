3e715132eb931b9d1e719042e2f98d0be04a2fdd
Nicolas <nicolasecrespo@gmail.com>
2024-04-28 19:27:41 -0400

    removed auto created keymaps of multicursor plugin

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -3,7 +3,10 @@ return {
   event = "LazyFile",
   config = function()
     vim.cmd([[silent! VMTheme codedark]])
+    -- remove useless keymaps
     vim.keymap.del("n", "\\\\\\")
     vim.keymap.del("n", "\\\\/")
+    vim.keymap.del("n", "\\\\gS")
+    vim.keymap.del("n", "\\\\A")
   end,
 }
