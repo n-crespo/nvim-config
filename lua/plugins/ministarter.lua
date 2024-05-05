cff7583a99f46962ba2d3a0665ff8b02295e53e9
Nicolas <nicolasecrespo@gmail.com>
2024-05-05 12:58:02 -0400

    remove j and k in mini starter

--- lua/plugins/ministarter.lua
+++ lua/plugins/ministarter.lua
@@ -54,12 +54,12 @@ return {
       },
     })
 
-    vim.cmd([[
-  augroup MiniStarterJK
-    au!
-    au User MiniStarterOpened nmap <buffer> j <Cmd>lua MiniStarter.update_current_item('next')<CR>
-    au User MiniStarterOpened nmap <buffer> k <Cmd>lua MiniStarter.update_current_item('prev')<CR>
-  augroup END
-]])
+    --     vim.cmd([[
+    --   augroup MiniStarterJK
+    --     au!
+    --     au User MiniStarterOpened nmap <buffer> j <Cmd>lua MiniStarter.update_current_item('next')<CR>
+    --     au User MiniStarterOpened nmap <buffer> k <Cmd>lua MiniStarter.update_current_item('prev')<CR>
+    --   augroup END
+    -- ]])
   end,
 }
