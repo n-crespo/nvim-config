b8ee7a3eee34c1123ae866286a9c83f287d21bf1
Nicolas <nicolasecrespo@gmail.com>
2024-05-05 14:00:57 -0400

    removed markdown preview in favor of peek

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -10,7 +10,7 @@ return {
       syntax = true, -- enable syntax highlighting, affects performance
       theme = "dark",
       update_on_change = true,
-      app = "xdg-open", -- 'webview', 'browser', string or a table of strings explained below
+      app = { "/mnt/c/Program Files/Mercury/mercury.exe", "--new-window" }, -- 'webview', 'browser', string or a table of strings explained below
     })
     -- vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
     -- vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
