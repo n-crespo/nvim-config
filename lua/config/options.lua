2f13ca4db833b54ca7a4197a2615f802ca056481
Nicolas <nicolasecrespo@gmail.com>
2024-05-05 15:10:23 -0400

    set custom tabline to remove X

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -41,3 +41,36 @@ vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazygit_config = true -- use custom layzgit config for icons/stuff
 vim.g.dark_bg = false -- custom option for custom 'macro' theme
 vim.g.python3_host_prog = "/usr/bin/python3"
+
+vim.cmd([[
+function! MyTabLine()
+  let s = ''
+  for i in range(tabpagenr('$'))
+    " select the highlighting
+    if i + 1 == tabpagenr()
+      let s .= '%#TabLineSel#'
+    else
+      let s .= '%#TabLine#'
+    endif
+
+    " set the tab page number (for mouse clicks)
+    let s .= '%' . (i + 1) . 'T'
+
+    " the label is made by MyTabLabel()
+    let s .= ' %{MyTabLabel(' . (i + 1) . ')} '
+  endfor
+
+  " after the last tab fill with TabLineFill and reset tab page nr
+  let s .= '%#TabLineFill#%T'
+
+  return s
+endfunction
+
+function! MyTabLabel(n)
+  let buflist = tabpagebuflist(a:n)
+  let winnr = tabpagewinnr(a:n)
+  return bufname(buflist[winnr - 1])
+endfunction
+
+set tabline=%!MyTabLine()
+]])
