4630bafff373dccb8d60d4298d5ae287a1b5be8e
Nicolas <nicolasecrespo@gmail.com>
2024-05-05 13:06:43 -0400

    add correct markdown peek keymap

--- lua/plugins/peek.lua
+++ lua/plugins/peek.lua
@@ -12,7 +12,8 @@ return {
       update_on_change = true,
       app = "xdg-open", -- 'webview', 'browser', string or a table of strings explained below
     })
-    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    -- vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    -- vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    vim.keymap.set("n", "<leader>op", require("peek").open, { desc = "Open Peek Preview", buffer = true })
   end,
 }
