f562066229859ddb8e901d6d0cb83edb4f5b46eb
Nicolas <nicolasecrespo@gmail.com>
2024-05-05 12:58:08 -0400

    add peek and add fix for WSL

---
+++ lua/plugins/peek.lua
@@ -0,0 +1,18 @@
+return {
+  "toppair/peek.nvim",
+  ft = "markdown",
+  -- event = { "VeryLazy" },
+  build = "deno task --quiet build:fast",
+  config = function()
+    require("peek").setup({
+      auto_load = true, -- whether to automatically load preview when entering another markdown buffer
+      close_on_bdelete = true, -- close preview window on buffer delete
+      syntax = true, -- enable syntax highlighting, affects performance
+      theme = "dark",
+      update_on_change = true,
+      app = "xdg-open", -- 'webview', 'browser', string or a table of strings explained below
+    })
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+  end,
+}
