0c1b0a92ec9dae57e373e7e91b99357b91ca2f42
Nicolas <nicolasecrespo@gmail.com>
2024-05-05 23:32:06 -0400

    cleaned up markdown config, added keymap for toggle markdown syntax

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,32 +2,37 @@
 -- to create links, <zh> to fold headers.
 return {
   "n-crespo/nvim-markdown",
-  lazy = true,
   ft = "markdown",
-  -- keys = {
-  --   { "<leader>t", "<cmd>Toc<cr><cmd>set nornu<cr><cmd>set nonu<cr>", desc = "Table of Contents" },
-  -- },
   config = function()
     vim.g.vim_markdown_toc_autofit = 1
-    vim.g.vim_markdown_math = 1
+    vim.g.vim_markdown_math = 0
     vim.cmd([[map zh <Plug>Markdown_Fold]])
     vim.cmd([[map <Plug> <Plug>Markdown_CreateLink]])
     vim.keymap.set(
       "n",
       "<leader>m",
-      "<cmd>setlocal syn=markdown<cr>",
+      function()
+        if vim.g.vim_markdown_math == 0 then
+          vim.cmd([[setlocal syn=markdown]])
+          vim.g.vim_markdown_math = 1
+        elseif vim.g.vim_markdown_math == 1 then
+          vim.cmd([[setlocal syn=]])
+          vim.g.vim_markdown_math = 0
+        end
+      end,
+      -- "<cmd>setlocal syn=markdown<cr>",
       { silent = false, desc = "Conceal Math", buffer = true }
     )
   end,
-  init = function()
-    -- for markdown math viewing
-    vim.api.nvim_create_autocmd({ "FileType", "BufReadPost" }, {
-      pattern = { "*.md" },
-      callback = function()
-        vim.cmd([[
-        setlocal syn=markdown
-        ]]) -- this will enable concealing inline math + some latex symbols
-      end,
-    })
-  end,
+  -- init = function()
+  --   -- for markdown math viewing
+  --   vim.api.nvim_create_autocmd({ "FileType", "BufReadPost" }, {
+  --     pattern = { "*.md" },
+  --     callback = function()
+  --       vim.cmd([[
+  --       setlocal syn=markdown
+  --       ]]) -- this will enable concealing inline math + some latex symbols
+  --     end,
+  --   })
+  -- end,
 }
