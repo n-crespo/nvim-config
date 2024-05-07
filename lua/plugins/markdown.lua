ac7bfe81ca25f2334a7d51af80abf7a0a3925a56
Nicolas <nicolasecrespo@gmail.com>
2024-05-06 21:18:05 -0400

    fixed math toggle keymap

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -5,34 +5,22 @@ return {
   ft = "markdown",
   config = function()
     vim.g.vim_markdown_toc_autofit = 1
-    vim.g.vim_markdown_math = 0
+    vim.g.vim_markdown_math = false
     vim.cmd([[map zh <Plug>Markdown_Fold]])
     vim.cmd([[map <Plug> <Plug>Markdown_CreateLink]])
     vim.keymap.set(
       "n",
       "<leader>m",
       function()
-        if vim.g.vim_markdown_math == 0 then
+        vim.g.vim_markdown_math = not vim.g.vim_markdown_math
+        if vim.g.vim_markdown_math then
           vim.cmd([[setlocal syn=markdown]])
-          vim.g.vim_markdown_math = 1
-        elseif vim.g.vim_markdown_math == 1 then
+        else
           vim.cmd([[setlocal syn=]])
-          vim.g.vim_markdown_math = 0
         end
       end,
       -- "<cmd>setlocal syn=markdown<cr>",
       { silent = false, desc = "Conceal Math", buffer = true }
     )
   end,
-  -- init = function()
-  --   -- for markdown math viewing
-  --   vim.api.nvim_create_autocmd({ "FileType", "BufReadPost" }, {
-  --     pattern = { "*.md" },
-  --     callback = function()
-  --       vim.cmd([[
-  --       setlocal syn=markdown
-  --       ]]) -- this will enable concealing inline math + some latex symbols
-  --     end,
-  --   })
-  -- end,
 }
