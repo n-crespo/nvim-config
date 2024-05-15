1f302cc32839dde36a793d173945a53e99e896e3
Nicolas <nicolasecrespo@gmail.com>
2024-05-15 08:34:38 -0400

    added insert mode bold keymap for markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,6 +3,7 @@ vim.keymap.set("i", "<C-e>", "```<cr><cr>```<up><up>", { buffer = true, silent =
 vim.cmd([[ setlocal commentstring=<!--\ %s\ --> ]])
 vim.keymap.set("i", "$", "$$<left>", { remap = false, silent = true, buffer = true })
 vim.keymap.set("i", "\\$", "\\$", { remap = false, silent = true, buffer = true })
+vim.keymap.set("i", "<C-b>", "****<left><left>", { remap = false, silent = true, buffer = true, desc = "Bold" })
 
 vim.keymap.set("n", "<leader>cs", "<cmd>Toc<cr><cmd>set nonu nornu<cr>", { buffer = true, silent = true })
 
