23225bedcaf88b4bc663b37db0e029a1a4594174
Nicolas <nicolasecrespo@gmail.com>
2024-05-19 21:57:13 -0400

    remove md table plugin

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -6,14 +6,3 @@ vim.keymap.set("i", "\\$", "\\$", { remap = false, silent = true, buffer = true 
 vim.keymap.set("i", "<C-b>", "****<left><left>", { remap = false, silent = true, buffer = true, desc = "Bold" })
 
 vim.keymap.set("n", "<leader>cs", "<cmd>Toc<cr><cmd>set nonu nornu<cr>", { buffer = true, silent = true })
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*.md" },
-  callback = function()
-    local save_cursor = vim.fn.getpos(".")
-    pcall(function()
-      vim.cmd([[TableModeRealign]])
-    end)
-    vim.fn.setpos(".", save_cursor)
-  end,
-})
