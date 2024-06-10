f911756212adb467919c1d0019cc6491de82beb4
Nicolas <nicolasecrespo@gmail.com>
2024-06-10 00:32:20 -0400

    remove comments

--- lua/plugins/reticle.lua
+++ lua/plugins/reticle.lua
@@ -2,7 +2,6 @@ return {
   "tummetott/reticle.nvim",
   event = "VeryLazy", -- optionally lazy load the plugin
   opts = {
-    -- add options here if you wish to override the default settings
     never = {
       cursorline = {
         "lazy",
