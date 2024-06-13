e06df4eac1e357b473edf88791220fd2feffff39
Nicolas <nicolasecrespo@gmail.com>
2024-06-13 16:16:12 -0400

    remove incline in favor of bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,13 +1,45 @@
 return {
   "akinsho/bufferline.nvim",
-  event = "VeryLazy",
+  event = "BufEnter",
   opts = {
     options = {
       mode = "tabs",
-      seperator_style = "thin",
       show_buffer_close_icons = false,
+      show_duplicate_prefix = true,
       show_close_icon = false,
       color_icons = true,
+      style_preset = require("bufferline").style_preset.no_italic, -- or bufferline.style_preset.minimal,
+      -- style_preset = {
+      --   require("bufferline").style_preset.no_italic,
+      -- },
+      numbers = function(opts)
+        return string.format("%s", opts.raise(opts.ordinal))
+      end,
+      separator_style = "thin",
+      custom_filter = function(buf_number, buf_numbers)
+        -- filter out filetypes you don't want to see
+        if vim.bo[buf_number].filetype ~= "minifiles" then
+          return false
+        end
+        if vim.bo[buf_number].filetype ~= "toggleterm" then
+          return false
+        end
+        if vim.bo[buf_number].filetype ~= "starter" then
+          return false
+        end
+        if vim.bo[buf_number].filetype ~= "lazy" then
+          return false
+        end
+      end,
     },
+  },
+  keys = {
+    { "<leader>bp", false },
+    { "<leader>br", false },
+    { "<leader>bP", false },
+    { "]b", false },
+    { "[b", false },
+    { "]B", false },
+    { "[B", false },
   },
 }
