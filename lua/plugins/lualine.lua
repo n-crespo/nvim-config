8e9d21a8e7239eed886373f6fe31fc8b57ef80a1
Nicolas <nicolasecrespo@gmail.com>
2024-06-13 16:15:54 -0400

    disable lualine on starter and fix time formatting

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,9 +10,12 @@ return {
     opts.options.section_separators = { left = "", right = "" }
     opts.options.component_separators = { left = "", right = "" }
     opts.options.theme = require("transparentlualine").theme
+    opts.options.disabled_filetypes = {
+      statusline = { "starter" },
+    }
     opts.sections.lualine_z = {
       function()
-        return "  " .. tostring(os.date("%I:%M %p")):gsub("^%s*0", " ") -- remove leading 0 and convert to string
+        return "  " .. tostring(os.date("%I:%M %p")):gsub("^%s*0", "") -- remove leading 0 and convert to string
       end,
     }
   end,
