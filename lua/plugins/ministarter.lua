62274b098b1ae5f595fdb1aedec00b7aa60c7600
Nicolas <nicolasecrespo@gmail.com>
2024-06-14 16:44:32 -0400

    fix: remove mini starter autocmd due to unexpected behavior

--- lua/plugins/ministarter.lua
+++ lua/plugins/ministarter.lua
@@ -63,13 +63,5 @@ return {
         end
       end,
     })
-
-    vim.cmd([[
-      augroup MiniStarterJK
-        au!
-        au User MiniStarterOpened nmap <buffer> j <Cmd>lua MiniStarter.update_current_item('next')<CR>
-        au User MiniStarterOpened nmap <buffer> k <Cmd>lua MiniStarter.update_current_item('prev')<CR>
-      augroup END
-    ]])
   end,
 }
