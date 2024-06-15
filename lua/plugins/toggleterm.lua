78c5629772814fa2a0f8bd0f0ea41c3e0f815502
Nicolas <nicolasecrespo@gmail.com>
2024-06-15 03:19:09 -0400

    fix: curve floating terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,16 +11,17 @@ return {
         return vim.o.columns * 0.5
       end
     end,
-    shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
     shading_factor = "0", -- the percentage by which to lighten terminal background, default: -30 (gets multiplied by -3 if background is light)
     start_in_insert = true,
     insert_mappings = false, -- whether or not the open mapping applies in insert mode
     terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
     direction = "horizontal", --| "vertical" | "tab" | "float",
+    float_opts = { border = "curved" },
     -- This field is only relevant if direction is set to 'float'
   },
   keys = {
     { "<C-S-Q>", "<cmd>ToggleTerm direction=vertical<cr>", desc = "ToggleTerm vertical", mode = { "n", "t" } },
     { "<C-Q>", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "ToggleTerm horizontal", mode = { "n", "t" } },
+    { "<C-/>", "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm float", mode = { "n", "t" } },
   },
 }
