83f3f3c5ced12ebde3dedbbe510d70e5bf11c725
Nicolas <nicolasecrespo@gmail.com>
2024-06-17 00:46:58 -0400

    fix: properly delete <space>, keymap for RPipe (r.nvim plugin)

--- lua/plugins/r.lua
+++ lua/plugins/r.lua
@@ -3,7 +3,6 @@ return {
   ft = { "r", "rmarkdown", "rmd" },
   lazy = true,
   opts = {
-    -- Create a table with the options to be passed to setup()
     R_args = { "--quiet", "--no-save" },
     disable_cmds = {
       "RSaveClose",
@@ -57,28 +56,24 @@ return {
       "RDSendParagraph",
       "RViewDFs",
       "RDputObj",
-      -- "RSendSelection",
-      -- "RDSendLine",
-      -- "RPipe",
+      "ROBToggle", -- object browser (?)
+      "RSendSelection",
+      "RPipe",
     },
     hook = {
       on_filetype = function()
-        -- This function will be called at the FileType event
-        -- of files supported by R.nvim. This is an
-        -- opportunity to create mappings local to buffers.
+        -- This function will be called at the FileType event of files supported by R.nvim.
+        -- This is an opportunity to create mappings local to buffers.
         vim.keymap.set("n", "<Enter>", "<Plug>RDSendLine", { buffer = true })
         vim.keymap.set("v", "<Enter>", "<Plug>RSendSelection", { buffer = true })
-        -- override default mapping to my own
+        vim.keymap.del("i", "<Space>,", { buffer = true })
         vim.keymap.set("n", "<leader>d", "yydd", { desc = "Delete to Paste Register", silent = true, buffer = true })
 
-        -- Increase the width of which-key to handle the longer r-nvim descriptions
-        local wk = require("which-key")
+        -- Increase the width of which-key to handle the longer r-nvim descriptions local wk = require("which-key")
         -- Workaround from https://github.com/folke/which-key.nvim/issues/514#issuecomment-1987286901
-        wk.register({
+        require("which-key").register({
           ["<localleader>"] = {
-            a = nil,
             c = { name = "+code/chunks", ["🚫"] = "which_key_ignore" },
-            k = { name = "+knit", ["🚫"] = "which_key_ignore" },
             r = { name = "+r", ["🚫"] = "which_key_ignore" },
             v = { name = "+view", ["🚫"] = "which_key_ignore" },
           },
@@ -87,14 +82,4 @@ return {
     },
     pdfviewer = "",
   },
-  keys = function()
-    -- only activates when first buffer is opened
-    vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
-      pattern = { "r", "rmd" },
-      callback = function()
-        vim.keymap.del("i", "<Space>,", { buffer = true })
-        vim.notify("space keymap deleted")
-      end,
-    })
-  end,
 }
