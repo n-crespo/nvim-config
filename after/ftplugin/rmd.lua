03cc7f30312e656d25f5bafe40a5d8b8494f5a5d
Nicolas <nicolasecrespo@gmail.com>
2024-06-17 00:07:51 -0400

    fix: force buffer local keymap settings for rmarkdown headers

--- after/ftplugin/rmd.lua
+++ after/ftplugin/rmd.lua
@@ -5,5 +5,5 @@ vim.keymap.set(
   "n",
   "<leader>rH",
   'i---<CR>title: ""<CR>author: "Nicolas Crespo"<CR>date: "`r Sys.Date()`"<CR>output: pdf_document<CR>---<CR><CR>```{r setup, include=FALSE}<CR>knitr::opts_chunk$set(echo = TRUE)<CR>```<CR><CR>---<CR><CR>',
-  { desc = "Rmarkdown Header" }
+  { desc = "Rmarkdown Header", buffer = true }
 )
