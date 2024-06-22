cf08d51308193eef1be63dde41d5a65049cce57a
Nicolas <nicolasecrespo@gmail.com>
2024-06-21 23:37:43 -0400

    feat: add incline

---
+++ lua/plugins/incline.lua
@@ -0,0 +1,41 @@
+return {
+  "b0o/incline.nvim",
+  event = "LazyFile",
+  config = function()
+    vim.g.showtabline = 0
+    local devicons = require("nvim-web-devicons")
+    require("incline").setup({
+      render = function(props)
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        if filename == "" then
+          filename = "[No Name]"
+        end
+
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
+
+        local function get_diagnostic_label()
+          local icons = { error = " ", warn = " ", info = " ", hint = " " }
+          local label = {}
+
+          for severity, icon in pairs(icons) do
+            local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+            if n > 0 then
+              table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
+            end
+          end
+          if #label > 0 then
+            table.insert(label, { "│ " })
+          end
+          return label
+        end
+
+        local modified = vim.bo[props.buf].modified and "  " or ""
+        return {
+          { get_diagnostic_label() },
+          { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
+          { filename .. "" .. modified },
+        }
+      end,
+    })
+  end,
+}
