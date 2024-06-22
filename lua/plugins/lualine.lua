6aa11cd637f300df7a0a1705276b43ff5474214f
Nicolas <nicolasecrespo@gmail.com>
2024-06-22 03:58:45 -0400

    feat(lualine)!: add tab page indicator

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -19,26 +19,29 @@ return {
     opts.sections.lualine_x = {
       -- stylua: ignore
       {
-        function() return require("noice").api.status.command.get() end,
-        cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-        color = function() return LazyVim.ui.fg("Statement") end,
-      },
-      -- stylua: ignore
-      {
-        function() return require("noice").api.status.mode.get() end,
-        cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-        color = function() return LazyVim.ui.fg("Constant") end,
-      },
-      {
         function()
           local tabs = {}
           local current_tab = vim.fn.tabpagenr()
           for i = 1, vim.fn.tabpagenr("$") do
-            if i == current_tab then
-              table.insert(tabs, string.format("%%#LualineTabActive#[%d]%%*", i))
-            else
-              table.insert(tabs, string.format("%%#LualineTabInactive#[%d]%%*", i))
+            local winnr = vim.fn.tabpagewinnr(i)
+            local winid = vim.fn.win_getid(winnr, i)
+            local bufnr = vim.fn.winbufnr(winid)
+            local bufname = vim.fn.bufname(bufnr)
+            local name = vim.fn.fnamemodify(bufname, ":t")
+            local icon, color =
+            require("nvim-web-devicons").get_icon(name, vim.fn.fnamemodify(bufname, ":e"), { default = true })
+            if name == "" then
+              name = "Empty"  -- Set name to "Empty" if it is empty
             end
+
+            local highlight_group = "LualineTabInactive"
+            if i == current_tab then
+              highlight_group = "LualineTabActive"
+            end
+
+            -- this is quite disgusting innit
+      local tab_display = string.format("[%%#%s#%s%%#%s# %s%%*]", color, icon, highlight_group, name)
+            table.insert(tabs, tab_display)
           end
           return table.concat(tabs, " ")
         end,
@@ -48,6 +51,24 @@ return {
           return vim.fn.tabpagenr("$") > 1
         end,
       },
+      -- -- the below show the most recently type command (for some reason)
+      -- {
+      --   function()
+      --     return require("noice").api.status.command.get()
+      --   end,
+      --   cond = function()
+      --     return package.loaded["noice"] and require("noice").api.status.command.has()
+      --   end,
+      --   color = function()
+      --     return LazyVim.ui.fg("Statement")
+      --   end,
+      -- },
+      -- -- stylua: ignore
+      -- {
+      --   function() return require("noice").api.status.mode.get() end,
+      --   cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+      --   color = function() return LazyVim.ui.fg("Constant") end,
+      -- },
       {
         require("lazy.status").updates,
         cond = require("lazy.status").has_updates,
