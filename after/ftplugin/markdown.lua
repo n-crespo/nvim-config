118eec5bedf9b17c9b00f854d5b32d4937f5cb78
Nicolas <nicolasecrespo@gmail.com>
2024-06-27 15:57:20 -0400

    feat: add expandable markdown abbreviations for formatter exclusion

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -6,3 +6,6 @@ vim.keymap.set("i", "\\$", "\\$", { remap = false, silent = true, buffer = true 
 vim.keymap.set("i", "<C-b>", "****<left><left>", { remap = false, silent = true, buffer = true, desc = "Bold" })
 
 vim.keymap.set("n", "<leader>cs", "<cmd>Toc<cr><cmd>set nonu nornu<cr>", { buffer = true, silent = true })
+
+vim.cmd([[inoreabbrev <buffer> ps <!-- prettier-ignore-start -->]])
+vim.cmd([[inoreabbrev <buffer> pe <!-- prettier-ignore-end -->]])
