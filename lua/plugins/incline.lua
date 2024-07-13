7776e09652f00ce7b9bef9d02b8c3c569346ab21
Nicolas <nicolasecrespo@gmail.com>
2024-07-12 22:09:44 -0400

    feat: move diagnostics from lualine to incline

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -19,12 +19,28 @@ return {
           filename = "[No Name]"
         end
 
+        local function get_diagnostic_label()
+          local icons = { error = " ", warn = " ", info = " ", hint = " " }
+          local label = {}
+
+          for severity, icon in pairs(icons) do
+            local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+            if n > 0 then
+              table.insert(label, { n .. " ", group = "DiagnosticSign" .. severity })
+            end
+          end
+          if #label > 0 then
+            table.insert(label, { "│ " })
+          end
+          return label
+        end
+
         local ft_icon, ft_color = devicons.get_icon_color(filename)
 
         local modified = vim.bo[props.buf].modified
 
         return {
-          -- { get_diagnostic_label() },
+          { get_diagnostic_label() },
           { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
           { (filename .. (modified and " " or "")), gui = modified and "bold" or "none" },
         }
