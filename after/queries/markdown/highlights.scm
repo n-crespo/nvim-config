64ac566fc9eb395c6e6cee2f8d4f55737a7ff612
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-07-23 08:38:03 -0400

    fix: return to regular markdown dashes

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -17,21 +17,21 @@
 ; •
 
 ; if you want rounded bullet: replace conceal char with • and uncomment offset lines
-([(list_marker_plus) (list_marker_star)]
-  @markup.list
-  ; (#offset! @markup.list 0 0 0 -1)
-  (#set! priority 1000)
-  (#set! conceal "◾"))
-([(list_marker_plus) (list_marker_star)]
-  @markup.list
-  (#any-of? @markup.list "+" "*")
-  (#set! conceal "◾"))
-((list_marker_minus)
-  @markup.list
-  ; (#offset! @markup.list 0 0 0 -1)
-  (#set! conceal "◾"))
-((list_marker_minus)
-  @markup.list
-  (#eq? @markup.list "-")
-  (#set! conceal "◾"))
-
+; ([(list_marker_plus) (list_marker_star)]
+;   @markup.list
+;   ; (#offset! @markup.list 0 0 0 -1)
+;   (#set! priority 1000)
+;   (#set! conceal "◾"))
+; ([(list_marker_plus) (list_marker_star)]
+;   @markup.list
+;   (#any-of? @markup.list "+" "*")
+;   (#set! conceal "◾"))
+; ((list_marker_minus)
+;   @markup.list
+;   ; (#offset! @markup.list 0 0 0 -1)
+;   (#set! conceal "◾"))
+; ((list_marker_minus)
+;   @markup.list
+;   (#eq? @markup.list "-")
+;   (#set! conceal "◾"))
+;
--- lua/plugins/headlines.lua
+++ lua/plugins/headlines.lua
@@ -7,15 +7,22 @@ return {
         headline_highlights = {},
         -- disable bullets for now. See https://github.com/lukas-reineke/headlines.nvim/issues/66
         bullets = {},
+        dash_string = "-",
         fat_headlines = false,
         codeblock_highlght = false,
       }
+      for i = 1, 6 do
+        local hl = "Headline" .. i
+        vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
+        table.insert(opts[ft].headline_highlights, hl)
+      end
     end
     return opts
   end,
   ft = { "markdown", "norg", "rmd", "org" },
   config = function(_, opts)
     -- PERF: schedule to prevent headlines slowing down opening a file
+    vim.cmd([[highlight Dash guibg=#D19A66 gui=bold]])
     vim.schedule(function()
       require("headlines").setup(opts)
       require("headlines").refresh()
