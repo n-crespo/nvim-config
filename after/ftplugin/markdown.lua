c068fe013308db0dda7bc45d7a5984738d915907
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-07-24 11:46:23 -0400

    feat: fix markdown toc keymap desc

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -6,7 +6,12 @@ vim.keymap.set("i", "$", "$$<left>", { remap = false, silent = true, buffer = tr
 vim.keymap.set("i", "\\$", "\\$", { remap = false, silent = true, buffer = true })
 -- vim.keymap.set("i", "<C-b>", "****<left><left>", { remap = false, silent = true, buffer = true, desc = "Bold" })
 
-vim.keymap.set("n", "<leader>cs", "<cmd>Toc<cr><cmd>set nonu nornu<cr>", { buffer = true, silent = true })
+vim.keymap.set(
+  "n",
+  "<leader>cs",
+  "<cmd>Toc<cr><cmd>set nonu nornu<cr>",
+  { buffer = true, silent = true, desc = "Table of Contents (Symbols)" }
+)
 
 vim.cmd([[inoreabbrev <buffer> ps <!-- prettier-ignore-start -->]])
 vim.cmd([[inoreabbrev <buffer> pe <!-- prettier-ignore-end -->]])
