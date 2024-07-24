799a6f17873596f9c58aad4976fabba412245970
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-07-24 11:49:57 -0400

    feat: render markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,12 +1,61 @@
 -- Use <enter> to follow markdown links (or hyperlinks), <C-k> in insert mode
 -- to create links, <zh> to fold headers.
 return {
-  "n-crespo/nvim-markdown",
-  ft = "markdown",
-  config = function()
-    vim.g.vim_markdown_toc_autofit = 1
-    vim.g.vim_markdown_math = false
-    vim.cmd([[map zh <Plug>Markdown_Fold]])
-    vim.cmd([[map <Plug> <Plug>Markdown_CreateLink]])
-  end,
+  {
+    "n-crespo/nvim-markdown",
+    ft = "markdown",
+    config = function()
+      vim.g.vim_markdown_toc_autofit = 1
+      vim.g.vim_markdown_math = false
+      vim.cmd([[map zh <Plug>Markdown_Fold]])
+      vim.cmd([[map <Plug> <Plug>Markdown_CreateLink]])
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+      },
+    },
+  },
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    opts = {
+      file_types = { "markdown", "norg", "rmd", "org" },
+      code = {
+        sign = false,
+        width = "full",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+    },
+    ft = { "markdown", "norg", "rmd", "org" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      LazyVim.toggle.map("<leader>um", {
+        name = "Render Markdown",
+        get = function()
+          return require("render-markdown.state").enabled
+        end,
+        set = function(enabled)
+          local m = require("render-markdown")
+          if enabled then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      })
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    enabled = false,
+  },
 }
