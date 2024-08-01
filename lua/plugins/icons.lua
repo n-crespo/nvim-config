7f7f0579587d1d2085fe74cb2de037aaf9acc8e8
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-07-31 23:08:11 -0400

    feat: cfml

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -1,17 +1,28 @@
 return {
-  "nvim-tree/nvim-web-devicons",
-  opts = {
-    override_by_extension = {
-      --  support .Rmd as well as .rmd
-      ["Rmd"] = {
-        icon = "",
-        color = "#519aba",
-        name = "RMarkdown",
+  {
+    "nvim-tree/nvim-web-devicons",
+    opts = {
+      override_by_extension = {
+        --  support .Rmd as well as .rmd
+        ["Rmd"] = {
+          icon = "",
+          color = "#519aba",
+          name = "RMarkdown",
+        },
+        ["cfm"] = {
+          icon = "",
+          color = "#7fb0ff",
+          name = "ColdFusion",
+        },
       },
-      ["cfm"] = {
-        icon = "",
-        color = "#7fb0ff",
-        name = "ColdFusion",
+    },
+  },
+  {
+    "echasnovski/mini.icons",
+    lazy = true,
+    opts = {
+      filetype = {
+        cf = { glyph = "", hl = "MiniIconsBlue" },
       },
     },
   },
