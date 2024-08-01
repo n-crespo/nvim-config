19846f284ca0f7aae1c698de61a8a6d06c3adf84
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-07-31 23:44:34 -0400

    remove startup time from mini.starter

--- lua/plugins/ministarter.lua
+++ lua/plugins/ministarter.lua
@@ -64,7 +64,7 @@ return {
         local stats = require("lazy").stats()
         local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
         -- local pad_footer = string.rep(" ", 8)
-        starter.config.footer = " " .. stats.loaded .. "/" .. stats.count .. " ⚡" .. ms .. "ms"
+        starter.config.footer = " " .. stats.loaded .. "/" .. stats.count -- ..  " ⚡" .. ms .. "ms"
         -- INFO; based on @echasnovski's recommendation (thanks a lot!!!)
         if vim.bo[ev.buf].filetype == "ministarter" then
           pcall(starter.refresh)
