e22370713fbed317fc1af6a0bbabf433215fc8a3
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-08-09 20:56:09 -0700

    testing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -311,3 +311,5 @@ vim.keymap.set("n", "<C-S-C>", function()
     title = "Word Count",
   })
 end)
+
+vim.keymap.set("n", "<leader>n", "<cmd>redir! > ~/.config/nvim/logs.txt<cr>", { silent = true })
