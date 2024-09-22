27adc5cd4e24edd556059c99ac5f8c7aa0268777
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-09-22 13:30:11 -0700

    use <C-n> and <C-p> for cycling qf list

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -129,3 +129,12 @@ vim.api.nvim_create_autocmd("ModeChanged", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function(event)
+    local opts = { buffer = event.buf, silent = true }
+    vim.keymap.set("n", "<C-n>", "<cmd>cn<CR>zz<cmd>wincmd p<CR>", opts)
+    vim.keymap.set("n", "<C-p>", "<cmd>cN<CR>zz<cmd>wincmd p<CR>", opts)
+  end,
+})
