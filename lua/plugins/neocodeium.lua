184b69b6e41daf775940cd18d52042b750700fb5
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-09-22 13:30:25 -0700

    fix neocodeium config

--- lua/plugins/neocodeium.lua
+++ lua/plugins/neocodeium.lua
@@ -1,4 +1,3 @@
--- TODO: Fix this config. When to disable? Fix Lualine component.
 return {
   "monkoose/neocodeium",
   event = "InsertEnter",
@@ -7,7 +6,7 @@ return {
     enabled = false, -- don't enable on start
     manual = false, -- require <C-n>? nah
     show_label = false, -- thing next to line numbers
-    silent = false, -- notification when server is started
+    silent = true, -- notification when server is started
     filetypes = {
       help = false,
       gitcommit = false,
@@ -19,33 +18,54 @@ return {
   config = function(_, opts)
     require("neocodeium").setup(opts)
 
+    -- refresh lualine whenever codeium server does something or plugin itself
+    -- is disabled
+    vim.api.nvim_create_autocmd("User", {
+      pattern = { "NeoCodeiumServer*", "NeoCodeium*{En,Dis}abled" },
+      callback = function()
+        require("lualine").refresh()
+      end,
+    })
+
+    -- when in insert mode, toggle completion only
     function ToggleCodeiumInsert()
-      local plugin, server = require("neocodeium").get_status()
-      if vim.g.neocodeium_enabled then
+      local plugin, _ = require("neocodeium").get_status()
+      if plugin > 0 then -- (completion is disabled)
         vim.cmd([[NeoCodeium enable]])
-        require("neocodeium").cycle_or_complete()
-      else
+        vim.notify("Codeium completion enabled", vim.log.levels.INFO, { title = "Codeium" })
+      else -- (completion is enabled)
+        vim.cmd([[NeoCodeium disable]]) -- disable completion
         require("neocodeium").clear()
+        vim.notify("Codeium completion disabled", vim.log.levels.WARN, { title = "Codeium" })
       end
     end
 
-    -- only start the codeium server if toggle is on AND in insert mode.
-    -- disable server once insert mode is left
-    -- vim.api.nvim_create_autocmd("InsertEnter", {
-    --   callback = function()
-    --     if vim.g.neocodeium_enabled then
-    --       vim.cmd([[NeoCodeium enable]])
-    --     end
-    --   end,
-    -- })
-    -- vim.api.nvim_create_autocmd("InsertLeave", {
-    --   callback = function()
-    --     vim.cmd([[NeoCodeium disable]])
-    --   end,
-    -- })
+    -- when in normal mode, keymap toggle servers
+    function ToggleCodeium()
+      local _, server = require("neocodeium").get_status()
+      if server == 2 then
+        vim.cmd([[NeoCodeium enable]])
+        vim.notify("NeoCodeium: the server has been started", vim.log.levels.INFO, { title = "Codeium" })
+      elseif server == 0 then
+        vim.cmd([[NeoCodeium! disable]])
+        -- vim.notify("Codeium server stopped...", vim.log.levels.WARN, { title = "Codeium" })
+      end
+    end
+
+    -- allow user to use :lua ToggleCodeium()
+    _G.ToggleCodeiumInsert = ToggleCodeiumInsert
+    _G.ToggleCodeium = ToggleCodeium
+
+    -- start codeium when entering insert mode (FOR THE FIRST TIME)
+    vim.api.nvim_create_autocmd("InsertEnter", {
+      once = true,
+      callback = function()
+        vim.cmd([[NeoCodeium enable]])
+      end,
+    })
   end,
   keys = {
-    -- in normal mode
+    -- toggles server (normal mode)
     {
       "<C-S-A>",
       function()
@@ -54,7 +74,7 @@ return {
       mode = "n",
       desc = "Toggle Codeium",
     },
-    -- in insert mode
+    -- toggles completion (insert mode)
     {
       "<C-S-A>",
       function()
@@ -62,13 +82,6 @@ return {
       end,
       mode = "i",
       desc = "Toggle Codeium",
-    },
-    {
-      "<C-a>",
-      function()
-        ToggleCodeiumInsert()
-      end,
-      mode = "i",
     },
     {
       "<C-CR>",
