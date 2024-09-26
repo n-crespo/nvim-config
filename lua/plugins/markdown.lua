b6ff05222b16680a1973cccb6aac61941c2f41ea
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-09-25 19:59:18 -0700

    render markdown in pending operator mode

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -24,7 +24,7 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
-      render_modes = { "n", "c", "i", "v", "V", "\x16", "t" },
+      render_modes = { "n", "c", "i", "v", "V", "\x16", "t", "o" },
       file_types = { "markdown", "norg", "rmd", "org" },
       code = {
         sign = false,
