7da47a95d3625e4464ff6aee47ab5cbd6cf74d84
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-10-20 22:55:02 -0700

    spell and remove web-devicons

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -1,29 +1,11 @@
 return {
-  {
-    "nvim-tree/nvim-web-devicons",
-    opts = {
-      override_by_extension = {
-        --  support .Rmd as well as .rmd
-        ["Rmd"] = {
-          icon = "",
-          color = "#519aba",
-          name = "RMarkdown",
-        },
-        ["cfm"] = {
-          icon = "",
-          color = "#7fb0ff",
-          name = "ColdFusion",
-        },
-      },
-    },
-  },
-  {
-    "echasnovski/mini.icons",
-    lazy = true,
-    opts = {
-      filetype = {
-        cf = { glyph = "", hl = "MiniIconsBlue" },
-      },
+  "echasnovski/mini.icons",
+  lazy = true,
+  opts = {
+    filetype = {
+      cf = { glyph = "", hl = "MiniIconsBlue" },
+      Rmd = { glyph = "", hl = "MiniIconsBlue" },
+      rmd = { glyph = "", hl = "MiniIconsBlue" },
     },
   },
 }
