0728acde0d58a91bf06b4b5fa39c801883b3b572
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-11-07 16:26:36 -0800

    remove junnk

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -310,9 +310,6 @@ vim.keymap.set("n", "<C-S-C>", function()
   })
 end)
 
-vim.keymap.set("n", "<leader>a", "<cmd>=require('neocodeium').visible()<cr>", { silent = false, desc = "AI Active" })
-vim.keymap.set("i", "<C-S-A>", "<cmd>=require('neocodeium').visible()<cr>", { silent = false, desc = "AI Active" })
-
 -- z=
 local spell_on_choice = vim.schedule_wrap(function(_, idx)
   if type(idx) == "number" then
