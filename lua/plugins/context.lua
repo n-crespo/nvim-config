7a89f6e6e99652b856f19aede754ca9e21ac547c
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-11-18 15:34:45 -0800

    disable context inside of terminal

--- lua/plugins/context.lua
+++ lua/plugins/context.lua
@@ -1,6 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   opts = {
-    multiwindow = true, -- Enable multiwindow support.
+    -- multiwindow = true, -- Enable multiwindow support.
+    highlight = {
+      enable = true, -- false will disable the whole extension
+      disable = { "snacks_terminal" }, -- list of language that will be disabled
+    },
   },
 }
