23b4dbd4fcdc437751fda95f7e397949eb16eded
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-12-04 01:53:17 -0800

    add pick session component to dashboard and json extra

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -17,6 +17,7 @@ return {
             action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
           },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "p", desc = "Pick Session", action = ':lua require("persistence").select()' },
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
