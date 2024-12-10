ee0235e8f0d33228de2c7229091aa9f713aaf35e
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-12-09 19:12:26 -0800

    edit keymaps for incremental treesitter node/scope selection/deselection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,9 +5,9 @@ return {
       enable = true,
       keymaps = {
         init_selection = "v<cr>", -- use S for this (flash)
-        scope_incremental = "<CR>",
-        node_incremental = "<TAB>",
-        node_decremental = "<S-TAB>",
+        scope_incremental = "<S-CR>",
+        node_incremental = "<CR>",
+        node_decremental = "<BS>",
       },
     },
     ensure_installed = {
