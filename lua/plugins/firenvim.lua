d23eacdd67ae03d430bdb5d169a7a7a291ed7ac8
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-12-12 21:58:47 -0800

    improve firenvim config

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -1,4 +1,11 @@
 return {
   "glacambre/firenvim",
+  lazy = false,
   build = ":call firenvim#install(0)",
+  config = function()
+    if vim.g.started_by_firenvim then
+      vim.o.laststatus = 0
+      vim.opt.guifont = "JetBrainsMono Nerd Font"
+    end
+  end,
 }
