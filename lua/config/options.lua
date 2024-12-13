630ddd3fc9c4d77fc0687f83fcb7bee80269880f
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-12-12 20:27:55 -0800

    dont animate snacks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -59,6 +59,7 @@ vim.g.loaded_python3_provider = 0
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.lazyvim_python_lsp = "pyright"
+vim.g.snacks_animate = false
 
 vim.cmd([[
 set complete=
