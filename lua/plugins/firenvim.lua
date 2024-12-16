b901cfcf1d04550f5666ed7cbf7c572cc14c3631
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-12-16 08:12:40 -0500

    disable more unneeded ui elements when using firenvim

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -1,12 +1,20 @@
 return {
   "glacambre/firenvim",
-  lazy = false,
+  lazy = true,
   build = ":call firenvim#install(0)",
+  module = false,
   config = function()
     if vim.g.started_by_firenvim then
+      local opt = vim.opt
       vim.o.laststatus = 0
-      vim.opt.guifont = "JetBrainsMono Nerd Font"
-      vim.opt.cursorline = false
+      opt.guifont = "JetBrainsMono Nerd Font"
+      opt.cursorline = false
+      opt.rnu = false
+      opt.nu = false
+      opt.signcolumn = "no"
+      opt.foldcolumn = "0"
+      opt.laststatus = 0
+      vim.keymap.set("i", "<CR>", "<cmd>wq<cr>", { buffer = true })
     end
   end,
 }
