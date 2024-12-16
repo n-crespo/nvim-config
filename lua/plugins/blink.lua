2f357aa2252ae2c1360ecce5fdb71b52c673b2a5
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-12-16 11:32:12 -0500

    fully replace cmp config with blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,4 +1,33 @@
 return {
   "saghen/blink.cmp",
-  opts = {},
+  opts = {
+    completion = {
+      ghost_text = {
+        enabled = false,
+      },
+      menu = {
+        draw = {
+          columns = {
+            { "label", "label_description", gap = 1 },
+            { "kind_icon", "kind" },
+          },
+        },
+      },
+      documentation = {
+        window = {
+          border = "single",
+          winhighlight = "Normal:BlinkCmpDoc,FloatBorder:CmpCompletionBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+        },
+      },
+    },
+    keymap = {
+      ["<CR>"] = { "select_and_accept", "fallback" },
+      ["<C-e>"] = { "hide", "show", "fallback" },
+      -- used by neocodeium
+      ["<C-n>"] = {},
+      ["<C-p>"] = {},
+      ["<C-j>"] = { "select_next" },
+      ["<C-k>"] = { "select_prev", "fallback" },
+    },
+  },
 }
