7922adb69fc48f8e9c6bb0ef7827caa5d088d108
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-12-16 09:46:50 -0500

    disable persistence on windows

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -1,5 +1,6 @@
 return {
   "folke/persistence.nvim",
+  enabled = not vim.fn.has("win32"),
   opts = {
     pre_save = function()
       vim.api.nvim_exec_autocmds("User", { pattern = "SessionSavePre" })
