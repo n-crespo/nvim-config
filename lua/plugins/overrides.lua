7ef1d07f5df231f9f544e1359b4a4871d7519a49
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-01-04 00:18:25 -0500

    truly disable tokyonight

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -12,4 +12,14 @@ return {
     "akinsho/bufferline.nvim",
     enabled = false,
   },
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "macro",
+    },
+  },
 }
