44b1a9cd8c7e19c85598354e7972794bfd9652aa
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-01-06 12:19:20 -0800

    update flatten.nvim config

--- lua/plugins/flatten.lua
+++ lua/plugins/flatten.lua
@@ -2,16 +2,13 @@ return {
   "willothy/flatten.nvim",
   enabled = true,
   priority = 99999,
-  commit = "7605dec",
   config = true,
   lazy = false,
   opts = {
-    window = {
-      open = "alternate",
-    },
-    callbacks = {
-      pre_open = function()
-        require("snacks").terminal.toggle()
+    window = { open = "tab" },
+    hooks = {
+      post_open = function()
+        vim.cmd([[silent! windo only]])
       end,
     },
   },
