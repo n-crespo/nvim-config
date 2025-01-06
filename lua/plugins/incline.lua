5ad6670be2a8ece98c66ab187f20122aecf35bb4
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-01-06 12:44:05 -0800

    move incline configuration to opts table rather than require function

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -15,34 +15,31 @@ return {
     },
   },
   enabled = not vim.g.started_by_firenvim,
-  config = function()
-    vim.g.showtabline = 0
-    require("incline").setup({
-      window = {
-        -- placement = { vertical = "top", horizontal = "center" },
-        options = { buftype = "nofile", wrap = false },
-        margin = { horizontal = 0, vertical = 1 },
-        overlap = { tabline = false, winbar = false },
-        width = "fit",
-      },
-      hide = {
-        cursorline = "focused_win",
-        only_win = false,
-      },
-      render = function(props)
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        if filename == "" then
-          filename = "[No Name]"
-        end
+  opts = {
+    window = {
+      -- placement = { vertical = "top", horizontal = "center" },
+      options = { buftype = "nofile", wrap = false },
+      margin = { horizontal = 0, vertical = 1 },
+      overlap = { tabline = false, winbar = false },
+      width = "fit",
+    },
+    hide = {
+      cursorline = "focused_win",
+      only_win = false,
+    },
+    render = function(props)
+      local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+      if filename == "" then
+        filename = "[No Name]"
+      end
 
-        local ft_icon, ft_color = require("mini.icons").get("file", filename)
-        local modified = vim.bo[props.buf].modified
-        return {
-          ft_icon and { (ft_icon or "") .. " ", group = ft_color, guibg = "none" } or "",
-          { filename, gui = modified and "bold" or "none" },
-          { modified and " " or "" },
-        }
-      end,
-    })
-  end,
+      local ft_icon, ft_color = require("mini.icons").get("file", filename)
+      local modified = vim.bo[props.buf].modified
+      return {
+        ft_icon and { (ft_icon or "") .. " ", group = ft_color, guibg = "none" } or "",
+        { filename, gui = modified and "bold" or "none" },
+        { modified and " " or "" },
+      }
+    end,
+  },
 }
