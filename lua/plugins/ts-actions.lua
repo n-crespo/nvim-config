096c5b2e1080f9fba28769887aaafee5066b08a8
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-01-06 12:49:40 -0800

    move ts-node-action keymap to keys table

--- lua/plugins/ts-actions.lua
+++ lua/plugins/ts-actions.lua
@@ -2,15 +2,17 @@ return {
   "ckolkey/ts-node-action",
   event = "LazyFile",
   dependencies = { "nvim-treesitter" },
-  opts = {},
-  config = function()
-    require("ts-node-action").setup({})
-    vim.keymap.set("n", "m", function()
-      local save_cursor = vim.fn.getpos(".")
-      pcall(function()
-        require("ts-node-action").node_action()
-      end)
-      vim.fn.setpos(".", save_cursor)
-    end, { desc = "Node Action" })
-  end,
+  keys = {
+    {
+      "m",
+      function()
+        local save_cursor = vim.fn.getpos(".")
+        pcall(function()
+          require("ts-node-action").node_action()
+        end)
+        vim.fn.setpos(".", save_cursor)
+      end,
+      { desc = "Node Action" },
+    },
+  },
 }
