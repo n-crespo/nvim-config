eb3b57fb60360e4b1dfda5be2e1859eeb59f3cb0
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-01-08 16:45:24 -0800

    fully disable <C-k> insert mode keymap for focusing signature help window

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,31 +1,20 @@
 -- lsp configurations
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    diagnostics = {
-      float = {
-        border = "single",
-      },
-    },
-    ui = {
-      default_options = {
-        border = "single",
-      },
-    },
-    document_highlight = {
-      enabled = false,
-    },
-    -- setup = {
-    -- jdtls = function()
-    --   return true -- avoid duplicate servers (??)
-    -- end,
-    -- },
-  },
-  keys = {
-    {
-      "<C-k>",
-      false,
-      mode = "i",
-    },
-  },
+  opts = function(_, opts)
+    -- disable <C-k> insert mode keymap for focusing signature help window
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = { "<C-K>", false, mode = "i" }
+
+    -- single border for floating diagnostic window
+    opts.diagnostics = opts.diagnostics or {}
+    opts.diagnostics.float = opts.diagnostics.float or {}
+    opts.diagnostics.float.border = "single"
+
+    opts.ui = opts.ui or {}
+    opts.ui.default_options = opts.ui.default_options or {}
+    opts.ui.default_options.border = "single"
+
+    return opts
+  end,
 }
