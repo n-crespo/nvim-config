08d7406e612b937ea338349641712cf919a61ef3
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-01-15 15:55:40 -0800

    disable incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "v<cr>", -- use S for this (flash)
-        scope_incremental = "<S-CR>",
-        node_incremental = "<CR>",
-        node_decremental = "<BS>",
-      },
-    },
+    incremental_selection = { enable = false },
     ensure_installed = {
-      "csv",
       "bash",
+      "csv",
       "yaml",
       "xml",
       "vimdoc",
