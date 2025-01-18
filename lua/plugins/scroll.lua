e1100549a4499d9a343aa127b5787428a630cd3c
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-01-18 14:28:20 -0800

    clean up scrollEOF config

--- lua/plugins/scroll.lua
+++ lua/plugins/scroll.lua
@@ -1,13 +1,10 @@
 return {
   "Aasim-A/scrollEOF.nvim",
-  enabled = true,
   event = "LazyFile",
   opts = {
     disabled_filetypes = {
       "minifiles",
       "minifiles-help",
-      "better_term",
-      "lazyterm",
       "snacks_terminal",
     },
   },
