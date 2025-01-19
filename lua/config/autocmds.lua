881b02d65f22d228134996e49cd56948cf8d3213
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-01-18 19:09:57 -0800

    fix cursorline autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -15,10 +15,11 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
   callback = function()
     if vim.w.auto_cursorline then
       vim.wo.cursorline = true
-      vim.w.auto_cursorline = nil
+      vim.w.auto_cursorline = false
     end
   end,
 })
+
 vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
   desc = "Enable cursorline only in inactive window",
   callback = function()
