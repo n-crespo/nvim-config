f826d3c9328b7fc9e4547e9d2cf0d4e42d4208f7
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-01-18 21:26:13 -0800

    close term on flatten

--- lua/plugins/flatten.lua
+++ lua/plugins/flatten.lua
@@ -8,6 +8,7 @@ return {
     window = { open = "tab" },
     hooks = {
       post_open = function()
+        require("snacks").terminal.toggle()
         vim.cmd([[silent! windo only]])
       end,
     },
