e6aab2a3e320ad5f5c976f4248282805f5dc396a
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-01-22 02:47:23 -0800

    fix: remove unused trouble keymaps

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/trouble.nvim",
+  keys = {
+    { "<leader>xL", nil },
+    { "<leader>xl", nil },
+    { "<leader>xt", nil },
+    { "<leader>xT", nil },
+    { "<leader>xq", nil },
+    { "<leader>xQ", nil },
+  },
+}
