0f59245b13664241bfda708dac5f35436645939c
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-02-16 16:29:12 -0800

    feat(dap)

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+  callback = function()
+    if vim.bo.filetype == "dap-float" then
+      vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = true })
+    end
+  end,
+})
+return {
+  "mfussenegger/nvim-dap",
+}
