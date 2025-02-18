e09d422383e5052181786235bca8b01fb119c7d2
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-02-18 03:44:20 -0800

    fix: disable image snack

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,14 +1,17 @@
 -- this doesn't work in windows terminal for some reason (yazi image preview
 -- does)
 return {
-  "folke/snacks.nvim",
-  ---@type snacks.Config
-  opts = {
-    image = {
-      force = false,
-      -- your image configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      image = {
+        enabled = false,
+        force = false,
+        -- your image configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
     },
   },
 }
