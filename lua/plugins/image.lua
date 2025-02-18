b99e9b69d98a07f953d86986b83e527e924a78a8
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-02-18 00:41:54 -0800

    feat(image): try to add image support

---
+++ lua/plugins/image.lua
@@ -0,0 +1,14 @@
+-- this doesn't work in windows terminal for some reason (yazi image preview
+-- does)
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    image = {
+      force = false,
+      -- your image configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+}
