e9ac4520885158e9f13717a41da6398ee978fa20
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-02-18 04:00:01 -0800

    fix(image): move filetype autocmds and custom keymaps to image config

--- after/ftplugin/image.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set("n", "<CR>", "<cmd>silent! !open %<cr>", { buffer = true, desc = "Open Image in System Viewer" })
--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,17 +1,49 @@
--- this doesn't work in windows terminal for some reason (yazi image preview
--- does)
+local formats = {
+  "png",
+  "jpg",
+  "jpeg",
+  "gif",
+  "bmp",
+  "webp",
+  "tiff",
+  "heic",
+  "avif",
+  "mp4",
+  "mov",
+  "avi",
+  "mkv",
+  "webm",
+  "pdf",
+}
+
+vim.api.nvim_create_autocmd({ "FileType", "BufReadCmd" }, {
+  desc = "Set image filetypes",
+  pattern = "*." .. table.concat(formats, ",*."),
+  callback = function(e)
+    Snacks.util.bo(e.buf, {
+      filetype = "image",
+      modifiable = false,
+      modified = false,
+      swapfile = false,
+    })
+  end,
+})
 return {
-  {
-    "folke/snacks.nvim",
-    ---@type snacks.Config
-    opts = {
-      image = {
-        enabled = false,
-        force = false,
-        -- your image configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      },
+  -- this doesn't work in windows terminal but sixel does (used in yazi)
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    image = {
+      enabled = false,
+    },
+  },
+  keys = {
+    {
+      "<CR>",
+      "<cmd>silent! !open %<CR>",
+      ft = "image",
+      buffer = true,
+      desc = "Open Image in System Viewer",
     },
   },
 }
