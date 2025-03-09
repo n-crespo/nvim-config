f995d58a99ac24988e7484e11db635a75d30e527
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-03-09 11:59:37 -0700

    feat(which-key): increase delay

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,13 +2,15 @@
 return {
   "folke/which-key.nvim",
   opts = {
-    preset = "helix",
     icons = { mappings = false }, -- disable icons
     filter = function(mapping)
       -- exclude mappings without a description
       return mapping.desc and mapping.desc ~= ""
     end,
-    show_help = true,
+    show_help = false,
+    delay = function(ctx)
+      return ctx.plugin and 0 or 350
+    end,
     plugins = {
       marks = false,
       spelling = {
