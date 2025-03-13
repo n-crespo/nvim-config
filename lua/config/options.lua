a6ded0a3cc5e30fd5d31225d7744f43d29c88692
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-03-12 19:30:58 -0700

    feat: better completeopt config for copilot chat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -21,6 +21,7 @@ opt.spelloptions = "noplainbuffer,camel"
 opt.showtabline = 0
 opt.cmdheight = 0 -- no cmdline on bottom of screen
 opt.wrap = false
+opt.completeopt = "menu,menuone,noselect,noinsert,popup"
 vim.g.loaded_ruby_provider = 0 -- never use these
 vim.g.loaded_perl_provider = 0 -- never use these
 vim.g.loaded_python3_provider = 0
