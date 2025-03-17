3c0edbd7ef769dfabd89a84ec20794b5f5a9c41d
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-03-16 22:59:48 -0700

    fix: clean up flatten pre_open function

--- lua/plugins/flatten.lua
+++ lua/plugins/flatten.lua
@@ -5,14 +5,10 @@ return {
   opts = {
     window = { open = "tab" },
     hooks = {
-      -- if file was opened from a terminal, close that terminal
       pre_open = function()
-        local win = vim.api.nvim_get_current_win()
-        local buf = vim.api.nvim_win_get_buf(win)
-        local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
-
-        if buftype == "terminal" then
-          vim.api.nvim_win_close(win, false)
+        -- if file was opened from a terminal, close that terminal
+        if vim.bo.buftype == "terminal" then
+          vim.api.nvim_win_close(0, false)
         end
       end,
     },
