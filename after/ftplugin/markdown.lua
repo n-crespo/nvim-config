e2cdd102438b468491e1a19564672fb61767d3ef
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-04-17 00:56:27 -0700

    fix(ftplugin): reorganize filetype specific configs for rmd/md

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,22 +1,15 @@
-vim.keymap.set("i", "<S-Tab>", "<C-d>", { buffer = true, silent = true, desc = "Decrease Indent" })
-vim.keymap.set("i", "<C-b>", "****<left><left>", { buffer = true, silent = true })
--- vim.cmd([[ setlocal commentstring=<!--\ %s\ --> ]])
-
+-- i no longer use prettier for markdown, cbfmt instead
 -- vim.cmd([[inoreabbrev <buffer> ps <!-- prettier-ignore-start -->]])
 -- vim.cmd([[inoreabbrev <buffer> pe <!-- prettier-ignore-end -->]])
 
--- NOTE; these save to the `r` register arbitrarily as to not clog up system clipboard
--- bold/italicize in visual mode
+-- NOTE; these save to the `r` register arbitrarily as to not clog up system
+-- clipboard bold/italicize in visual mode
+vim.keymap.set("i", "<C-b>", "****<left><left>", { buffer = true, silent = true })
 vim.keymap.set("x", "<C-b>", '"rc****<esc>h"rP', { buffer = true, desc = "Bold" })
 vim.keymap.set("x", "<C-i>", '"rc__<esc>"rP', { buffer = true, desc = "Italicize" })
 vim.opt.foldmethod = "expr" -- folds headers and lists nicely
 
--- check if current zone is table
--- find count of columns from | - | - |
--- for each column find max width for each cell (including padding)
--- for each column from start (get value from cell -> align it -> add padding -> goto next cell until everything done)
---
-
+-- this is a fairly rudimentary table formatting support on save
 local pattern = "^%s*|.*|%s*$"
 
 local function isInTable(line)
--- after/ftplugin/rmd.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set("i", "<C-e>", "```{r}<cr><cr>```<up>", { buffer = true, silent = true, desc = "R Code Block" })
-
-vim.keymap.set(
-  "n",
-  "<leader>rH",
-  'i---<CR>title: ""<CR>author: "Nicolas Crespo"<CR>date: "`r Sys.Date()`"<CR>output: pdf_document<CR>---<CR><CR>```{r setup, include=FALSE}<CR>knitr::opts_chunk$set(echo = TRUE)<CR>```<CR><CR>---<CR><CR>',
-  { desc = "Rmarkdown Header", buffer = true }
-)
