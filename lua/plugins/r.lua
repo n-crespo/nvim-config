54f1ac1942c969c01b3b2f197e625183fea5dcc0
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-04-21 17:40:46 -0700

    fix(r): clean up config and remove old keymap workaround

--- lua/plugins/r.lua
+++ lua/plugins/r.lua
@@ -2,13 +2,13 @@ M = {}
 
 if vim.fn.executable("R") == 1 then
   M = {
+    -- most of this is stolen from the extras/lang/r.lus from LazyVim
     {
       "R-nvim/R.nvim",
       ft = { "r", "rmarkdown", "rmd" },
       lazy = true,
       cond = vim.fn.executable("R") == 1,
       opts = {
-        pipe_keymap = "",
         R_args = { "--quiet", "--no-save" },
         disable_cmds = {
           "RSaveClose",
@@ -69,25 +69,26 @@ if vim.fn.executable("R") == 1 then
         },
         hook = {
           on_filetype = function()
-            -- This function will be called at the FileType event of files supported by R.nvim.
-            -- This is an opportunity to create mappings local to buffers.
-            vim.keymap.set("n", "<CR>", "<Plug>RDSendLine", { buffer = true })
-            vim.keymap.set(
-              "n",
-              "<leader>d",
-              "yydd",
-              { desc = "Delete to Paste Register", silent = true, buffer = true }
-            )
-
             vim.keymap.set("n", "<Enter>", "<Plug>RDSendLine", { buffer = true })
             vim.keymap.set("v", "<Enter>", "<Plug>RSendSelection", { buffer = true })
 
-            -- Increase the width of which-key to handle the longer r-nvim descriptions local wk = require("which-key")
-            -- Workaround from https://github.com/folke/which-key.nvim/issues/514#issuecomment-1987286901
-            require("which-key").add({
-              { "<leader>c", group = "code/chunks" },
-              { "<leader>r", group = "r general" },
-              { "<leader>v", group = "view" },
+            local wk = require("which-key")
+            wk.add({
+              buffer = true,
+              mode = { "n", "v" },
+              { "<localleader>a", group = "all" },
+              { "<localleader>b", group = "between marks" },
+              { "<localleader>c", group = "code/chunks" },
+              { "<localleader>f", group = "functions" },
+              { "<localleader>g", group = "goto" },
+              { "<localleader>i", group = "install" },
+              { "<localleader>k", group = "knit" },
+              { "<localleader>p", group = "paragraph" },
+              { "<localleader>q", group = "quarto" },
+              { "<localleader>r", group = "r general" },
+              { "<localleader>s", group = "split or send" },
+              { "<localleader>t", group = "terminal" },
+              { "<localleader>v", group = "view" },
             })
           end,
         },
@@ -99,7 +100,6 @@ if vim.fn.executable("R") == 1 then
         require("r.pdf.generic").open = vim.ui.open
       end,
     },
-    -- stolen from the extras/lang/r.lus from LazyVim
     {
       "neovim/nvim-lspconfig",
       opts = {
