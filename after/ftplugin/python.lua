b08ecc28660a2d0ab14fdf090c369a24005efce1
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-04-24 15:19:28 -0700

    fix(python): only try to load pylsp config if lspconfig is available

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,11 +1,13 @@
-require("lspconfig").pylsp.setup({
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {
-          ignore = { "E501" }, -- this disables errors about lines being too long
+if package.loaded["lspconfig"] then
+  require("lspconfig").pylsp.setup({
+    settings = {
+      pylsp = {
+        plugins = {
+          pycodestyle = {
+            ignore = { "E501" }, -- this disables errors about lines being too long
+          },
         },
       },
     },
-  },
-})
+  })
+end
