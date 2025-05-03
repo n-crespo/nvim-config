0be4625de2253e9f90e82482921284e7ad4c5ba0
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-02 23:00:24 -0700

    fix(options): remove unused options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,45 +1,38 @@
--- opts set by default lazyvim: LazyVim/lua/lazyvim/config/options.lua
+-- opts set by default lazyvim: ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/config/options.lua
 
 local opt = vim.opt
 opt.cursorlineopt = "number,screenline" -- cursorline respects wrapped lines
--- opt.showtabline = 2
 opt.laststatus = 3
 opt.relativenumber = false
 opt.swapfile = false -- don't make backup swap files
-opt.incsearch = true -- who knows
 opt.scrolloff = 8 -- don't scroll all the way down
 opt.sidescrolloff = 7 -- see :h sidescrolloff
 opt.sidescroll = 0 -- see help pages
 opt.numberwidth = 1 -- left side width
 opt.textwidth = 80 -- formatted text width
 opt.softtabstop = 2 -- 2 space tabs
-opt.pumblend = 0 -- needed for cmp transparency
+opt.pumblend = 0 -- some transparency
 opt.startofline = true
 opt.breakindent = true -- indent smartly
 opt.smartcase = true -- casing in search
 opt.ignorecase = true -- casing in search
-opt.spelllang = "en" -- spell in english pls
 opt.spelloptions = "noplainbuffer,camel"
-opt.cmdheight = 0 -- no cmdline on bottom of screen
-opt.wrap = false
 opt.completeopt = "menu,menuone,noselect,noinsert,popup"
-opt.foldtext = "v:lua.require'custom.utils'.custom_foldtext()" -- see ../custom/utils.lua
-vim.g.loaded_ruby_provider = 0 -- never use these
-vim.g.loaded_perl_provider = 0 -- never use these
+-- opt.autochdir = true -- this breaks things
+vim.g.loaded_ruby_provider = 0 -- never use this
+vim.g.loaded_perl_provider = 0 -- never use this
 vim.g.loaded_python3_provider = "/usr/bin/python3"
 vim.g.python3_host_prog = "/usr/bin/python3"
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.g.lazyvim_python_lsp = "pylsp"
-vim.g.root_spec = { ".root", "lsp", ".git", "cwd" }
-
--- opt.autochdir = true -- this breaks things
+vim.g.root_spec = { ".root", "lsp", { ".git", "lua" }, "cwd" }
 
 if LazyVim.is_win() then
   LazyVim.terminal.setup("powershell")
 end
 
---- for some reason this doesn't work unless I specify `== 1`???
+-- not needed anymore
 -- if vim.fn.has("wsl") == 1 then
 --   ---@diagnostic disable-next-line: undefined-field
 --   opt.clipboard:append("unnamedplus") -- use system clipboard as default register
