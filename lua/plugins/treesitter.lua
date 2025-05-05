ab892ddc8f09da3f4633dc26b767a21c6763912e
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-04 20:43:34 -0700

    fix(objump): use ts syntax instead

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,6 +21,7 @@ return {
       "xml",
       "vimdoc",
       "css",
+      "objdump",
     },
   },
 }
---
+++ queries/objdump/highlights.scm
@@ -0,0 +1,3 @@
+; extends
+;; lower comment priority so it loses against other captures
+((comment) @comment.documentation (#set! "priority" 150))
