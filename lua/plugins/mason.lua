c85358f5884e0bbc6d6bedc814647b266a7a93e8
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-08 00:42:32 -0700

    fix(lazy): pin old version of mason/mason-lsp-config (v2)

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,12 +1,20 @@
 -- makes installing and managing language servers a lot easier
 return {
-  "williamboman/mason.nvim",
-  event = "LspAttach",
-  opts = {
-    ui = {
-      border = "rounded",
-      width = 0.8,
-      height = 0.8,
+  {
+    "williamboman/mason.nvim",
+    event = "LspAttach",
+    opts = {
+      ui = {
+        border = "rounded",
+        width = 0.8,
+        height = 0.8,
+      },
     },
   },
+  -- mason and mason-lspconfig released version 2.0.
+  -- with some breaking changes, multiple methods have been changed
+  -- therefore (for now) a workaround is needed for Mason to still work in LazyVim
+  -- THIS WILL PIN th Version number, remove later, when no longer needed
+  { "mason-org/mason.nvim", version = "1.11.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
 }
