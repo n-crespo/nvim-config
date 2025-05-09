9651573054ff4e378153822421992af1355eaabd
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-09 01:17:16 -0700

    feat(session): auto load session when neovim started without args

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -15,23 +15,24 @@ return {
     },
   },
   -- https://github.com/LazyVim/LazyVim/discussions/5462
-  -- {
-  --   "folke/persistence.nvim",
-  --   lazy = false, -- make persistence start on startup
-  --   opts = function()
-  --     -- Auto restore session
-  --     vim.api.nvim_create_autocmd("VimEnter", {
-  --       nested = true,
-  --       callback = function()
-  --         local persistence = require("persistence")
-  --         if vim.fn.argc() == 0 and not require("custom.utils").is_man_pager() and not vim.g.started_with_stdin then
-  --           persistence.load()
-  --           -- else
-  --           -- persistence.stop()
-  --           -- you can use above to ignore sessions when neovim is opened with args
-  --         end
-  --       end,
-  --     })
-  --   end,
-  -- },
+  -- load the last saved session when neovim is opened with no args
+  {
+    "folke/persistence.nvim",
+    lazy = false, -- make persistence start on startup
+    opts = function()
+      -- Auto restore session
+      vim.api.nvim_create_autocmd("VimEnter", {
+        nested = true,
+        callback = function()
+          local persistence = require("persistence")
+          if vim.fn.argc() == 0 and not require("custom.utils").is_man_pager() and not vim.g.started_with_stdin then
+            persistence.load()
+            -- else
+            -- persistence.stop()
+            -- you can use above to discard sessions when neovim is opened with args
+          end
+        end,
+      })
+    end,
+  },
 }
