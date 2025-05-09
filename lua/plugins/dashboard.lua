ba0b63022bdc7d0f0d307f1c4ba4727f92a809a1
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-09 01:17:04 -0700

    feat(dashboard): reduce spacing

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -35,7 +35,7 @@ return {
       },
       sections = {
         { section = "header", padding = 1 },
-        { section = "keys", gap = 1, padding = 1 },
+        { section = "keys", gap = 0, padding = 1 },
         { section = "startup" },
       },
     },
