2d4d0901c194527942402ac66c052250f82e2349
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-12 04:40:00 -0700

    fix(lsp): disable marksman by default

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,20 +4,31 @@ vim.diagnostic.config({
 })
 
 return {
-  "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- disable <C-k> insert mode keymap for focusing signature help window
-    keys[#keys + 1] = { "<C-K>", false, mode = "i" }
-    -- disable some other keymaps
-    keys[#keys + 1] = { "<leader>cA", false, mode = "n" }
-    keys[#keys + 1] = { "<leader>cc", false, mode = "n" }
-    keys[#keys + 1] = { "<leader>cC", false, mode = "n" }
-    keys[#keys + 1] = { "<leader>cR", false, mode = "n" }
-    keys[#keys + 1] = { "<M-n>", false, mode = "n" }
-    keys[#keys + 1] = { "<M-p>", false, mode = "n" }
-    keys[#keys + 1] = { "]]", false, mode = "n" }
-    keys[#keys + 1] = { "[[", false, mode = "n" }
-    return opts
-  end,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- enable when needed... this is overkill really for just markdown files
+        marksman = { enabled = false },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable <C-k> insert mode keymap for focusing signature help window
+      keys[#keys + 1] = { "<C-K>", false, mode = "i" }
+      -- disable some other keymaps
+      keys[#keys + 1] = { "<leader>cA", false, mode = "n" }
+      keys[#keys + 1] = { "<leader>cc", false, mode = "n" }
+      keys[#keys + 1] = { "<leader>cC", false, mode = "n" }
+      keys[#keys + 1] = { "<leader>cR", false, mode = "n" }
+      keys[#keys + 1] = { "<M-n>", false, mode = "n" }
+      keys[#keys + 1] = { "<M-p>", false, mode = "n" }
+      keys[#keys + 1] = { "]]", false, mode = "n" }
+      keys[#keys + 1] = { "[[", false, mode = "n" }
+      return opts
+    end,
+  },
 }
