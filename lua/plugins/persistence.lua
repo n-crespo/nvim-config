6565c56bb2583224f20751bcb5962b42a37b5b90
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-11 22:40:39 -0700

    fix(sessions): only auto restore sessions in main config

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -20,19 +20,21 @@ return {
     "folke/persistence.nvim",
     lazy = false, -- make persistence start on startup
     opts = function()
-      -- Auto restore session
-      vim.api.nvim_create_autocmd("VimEnter", {
-        nested = true,
-        callback = function()
-          local persistence = require("persistence")
-          if vim.fn.argc() == 0 and not require("custom.utils").is_man_pager() and not vim.g.started_with_stdin then
-            persistence.load()
-            -- else
-            -- persistence.stop()
-            -- you can use above to discard sessions when neovim is opened with args
-          end
-        end,
-      })
+      -- Auto restore session (in main config)
+      if vim.env.NVIM_FULL_CONFIG == "true" then
+        vim.api.nvim_create_autocmd("VimEnter", {
+          nested = true,
+          callback = function()
+            local persistence = require("persistence")
+            if vim.fn.argc() == 0 and not require("custom.utils").is_man_pager() and not vim.g.started_with_stdin then
+              persistence.load()
+              -- else
+              -- persistence.stop()
+              -- you can use above to discard sessions when neovim is opened with args
+            end
+          end,
+        })
+      end
     end,
   },
 }
