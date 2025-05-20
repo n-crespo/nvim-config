b2ffe228d45474735a870fde959f4c21ff57551d
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-19 23:47:58 -0700

    feat: dim b and y sections of lualine

--- lua/lualine/themes/gray.lua
+++ lua/lualine/themes/gray.lua
@@ -1,41 +1,42 @@
 local dark_gray = "#201d1d"
 local almost_white = "#C5C9C5"
 local light_grey = "#2e2e2e"
+local subtle_grey = "#706F6D"
 
 return {
   inactive = {
     a = { fg = almost_white, bg = light_grey, bold = true },
-    b = { fg = almost_white, bg = dark_gray },
+    b = { fg = subtle_grey, bg = dark_gray },
     c = { fg = almost_white, bg = nil },
     d = { bg = light_grey },
   },
   visual = {
     a = { fg = almost_white, bg = light_grey, bold = true },
-    b = { fg = almost_white, bg = dark_gray },
+    b = { fg = subtle_grey, bg = dark_gray },
     c = { fg = almost_white, bg = nil },
     d = { bg = light_grey },
   },
   replace = {
     a = { fg = almost_white, bg = light_grey, bold = true },
-    b = { fg = almost_white, bg = dark_gray },
+    b = { fg = subtle_grey, bg = dark_gray },
     c = { fg = almost_white, bg = nil },
     d = { bg = light_grey },
   },
   normal = {
     a = { fg = almost_white, bg = light_grey, bold = true },
-    b = { fg = almost_white, bg = dark_gray },
+    b = { fg = subtle_grey, bg = dark_gray },
     c = { fg = almost_white, bg = nil },
     d = { bg = light_grey },
   },
   insert = {
     a = { fg = almost_white, bg = light_grey, bold = true },
-    b = { fg = almost_white, bg = dark_gray },
+    b = { fg = subtle_grey, bg = dark_gray },
     c = { fg = almost_white, bg = nil },
     d = { bg = light_grey },
   },
   command = {
     a = { fg = almost_white, bg = light_grey, bold = true },
-    b = { fg = almost_white, bg = dark_gray },
+    b = { fg = subtle_grey, bg = dark_gray },
     c = { fg = almost_white, bg = nil },
     d = { bg = light_grey },
   },
