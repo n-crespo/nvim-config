fbce7e62470232ac443d2b03e3d3b4a0a698313b
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-05-19 23:54:43 -0700

    feat: don't auto load persistent sessions

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -14,27 +14,31 @@ return {
       { "<leader>qS", false },
     },
   },
-  -- https://github.com/LazyVim/LazyVim/discussions/5462
+  -- i just do this manually with an alias to
+  --
+  -- nvim -c "lua require('persistence').load()"
+  --
   -- load the last saved session when neovim is opened with no args
-  {
-    "folke/persistence.nvim",
-    lazy = false, -- make persistence start on startup
-    opts = function()
-      -- Auto restore session (in main config)
-      if vim.g.full_config then
-        vim.api.nvim_create_autocmd("VimEnter", {
-          nested = true,
-          callback = function()
-            local persistence = require("persistence")
-            if vim.fn.argc() == 0 and not require("custom.utils").is_man_pager() and not vim.g.started_with_stdin then
-              persistence.load()
-              -- else
-              -- persistence.stop()
-              -- you can use above to discard sessions when neovim is opened with args
-            end
-          end,
-        })
-      end
-    end,
-  },
+  -- https://github.com/LazyVim/LazyVim/discussions/5462
+  -- {
+  --   "folke/persistence.nvim",
+  --   lazy = false, -- make persistence start on startup
+  --   opts = function()
+  --     -- Auto restore session (in main config)
+  --     if vim.g.full_config then
+  --       vim.api.nvim_create_autocmd("VimEnter", {
+  --         nested = true,
+  --         callback = function()
+  --           local persistence = require("persistence")
+  --           if vim.fn.argc() == 0 and not require("custom.utils").is_man_pager() and not vim.g.started_with_stdin then
+  --             persistence.load()
+  --             -- else
+  --             -- persistence.stop()
+  --             -- you can use above to discard sessions when neovim is opened with args
+  --           end
+  --         end,
+  --       })
+  --     end
+  --   end,
+  -- },
 }
